985d5380b5234eda00c84ce547912e6ec07e3181
Dmitriy <dmed832@yandex.ru>
2022-10-13 23:47:33 +0300

    add solution JavaSyntax from task0109

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0109/Solution.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task01.task0109;
+
+/* 
+«Я по объявлению…»
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int a = 5, b = 7;
+    }
+}
