ba00d741118bc236a15e3f9ffafb31d218180a1e
Dmitriy <dmed832@yandex.ru>
2022-10-13 23:54:52 +0300

    add solution JavaSyntax from task0103

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task01/task0103/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.pro.task01.task0103;
+
+/* 
+Сбросить Диего и сверхсекретный груз
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String drop = "Сбрасываем";
+        String diego = "Диего";
+        String secret = "сверхсекретный";
+        String cargo = "груз";
+
+
+        System.out.print(drop);
+        System.out.print(" Диего");
+        System.out.println();
+        System.out.print(drop);
+        System.out.print(" сверхсекретный ");
+        System.out.print(cargo);
+    }
+}
