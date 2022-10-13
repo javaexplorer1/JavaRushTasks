533fd5dd26c45e092635c628e0306b91f8f18db9
Dmitriy <dmed832@yandex.ru>
2022-10-13 23:42:32 +0300

    add solution JavaSyntax from task0105

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0105/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task01.task0105;
+
+/* 
+Объявляем переменные
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String name;
+        int age;
+        String city;
+    }
+}
