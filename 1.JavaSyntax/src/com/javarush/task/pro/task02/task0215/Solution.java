84d11818525541c8771e0b9573a4b73619257e21
Dmitriy <dmed832@yandex.ru>
2022-10-14 23:49:20 +0300

    add solution JavaSyntax from pro.task02.task0214

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0215/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task02.task0215;
+
+import java.util.Scanner;
+
+/* 
+Чтение чисел
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number1 = scanner.nextInt();
+        int number2 = scanner.nextInt();
+        int number3 = scanner.nextInt();
+        int avg = (number1 + number2 + number3) / 3;
+        System.out.println(avg);
+
+    }
+}
