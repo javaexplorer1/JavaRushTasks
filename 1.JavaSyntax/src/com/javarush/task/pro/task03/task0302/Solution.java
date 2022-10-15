0103f3cca03edf7b483790d22576a97b5265c8bd
Dmitriy <dmed832@yandex.ru>
2022-10-15 13:24:37 +0300

    add solution JavaSyntax from pro.task03.task0302

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0302/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task03.task0302;
+
+import java.util.Scanner;
+
+/* 
+Призывная кампания
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String militaryCommissar = ", явитесь в военкомат";
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        int age = scanner.nextInt();
+        if (age >= 18 && age <= 28) {
+            System.out.println(name + militaryCommissar);
+
+        }
+    }
+}
