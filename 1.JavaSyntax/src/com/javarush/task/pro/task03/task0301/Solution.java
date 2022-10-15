785f3a86e67a3c44d0c49b8598f651314a8d7805
Dmitriy <dmed832@yandex.ru>
2022-10-15 13:24:13 +0300

    add solution JavaSyntax from pro.task03.task0301

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0301/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.pro.task03.task0301;
+
+import java.util.Scanner;
+
+/* 
+Тепло или холодно
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String cold = "на улице холодно";
+        String warm = "на улице тепло";
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        if (number < 0) {
+            System.out.println(cold);
+        } else {
+            System.out.println(warm);
+        }
+    }
+}
