388d214031482b4cabad1432294cf1105f81fda1
Dmitriy <dmed832@yandex.ru>
2022-10-15 13:23:27 +0300

    add solution JavaSyntax from pro.task02.task0219

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0219/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task02.task0219;
+
+/* 
+Баги робокопа
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int id = 1, age = 30;
+        String documents = "citizen", behavior = "left";
+    }
+}
