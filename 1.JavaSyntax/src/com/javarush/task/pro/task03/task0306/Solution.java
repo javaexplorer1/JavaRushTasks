46f6fcb36906ff0b1174f933c79cda97a6e8b771
Dmitriy <dmed832@yandex.ru>
2022-10-15 13:24:49 +0300

    add solution JavaSyntax from pro.task03.task0306

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0306/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.pro.task03.task0306;
+
+import java.util.Scanner;
+
+/* 
+Треугольник
+*/
+
+public class Solution {
+    private static final String TRIANGLE_EXISTS = "треугольник существует";
+    private static final String TRIANGLE_NOT_EXISTS = "треугольник не существует";
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        if (a < (b + c) && b < (a + c) && c < (a + b)) {
+            System.out.println(TRIANGLE_EXISTS);
+        } else {
+            System.out.println(TRIANGLE_NOT_EXISTS);
+        }
+
+    }
+}
