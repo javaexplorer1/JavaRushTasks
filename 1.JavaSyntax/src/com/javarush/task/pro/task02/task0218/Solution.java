bf41e1482f5ecc480a223d82a23117dc0d58b4cc
Dmitriy <dmed832@yandex.ru>
2022-10-15 13:22:39 +0300

    add solution JavaSyntax from pro.task02.task0218

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0218/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task02.task0218;
+
+import java.util.Scanner;
+
+/* 
+Ручное управление
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number1 = scanner.nextInt();
+        int number2 = scanner.nextInt();
+        int number3 = scanner.nextInt();
+        System.out.println(number1 * number2 * number3);
+
+    }
+}
