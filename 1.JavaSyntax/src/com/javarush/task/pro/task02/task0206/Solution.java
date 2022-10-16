a40343547791ac013e3aeeb63610367ffa96b22c
Dmitriy <dmed832@yandex.ru>
2022-10-16 21:00:31 +0300

    add solution JavaSyntax from pro.task02.task0206

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0206/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task02.task0206;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String word = "переменная";
+        String phrase = "это фраза";
+        String line = "_____________________________";
+        String text = "это какой-то большой текст";
+
+    }
+}
