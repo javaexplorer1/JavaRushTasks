51a58b6bba5d1fd6b0cb1c58bbd7b7f660934fed
Dmitriy <dmed832@yandex.ru>
2022-10-16 21:18:46 +0300

    add solution JavaSyntax from pro.task02.task0213

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0213/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task02.task0213;
+
+/* 
+Причешем резюме
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String title = "Senior Lead Principal Software Engineer Data Architect";
+        String degree = "In college, I Majored in Political Science and Minored in Religious Studies.";
+        String career = "Experienced Team Leader with strong Organizational Skills and a Successful career in Management.";
+
+        //напишите тут ваш код
+        System.out.println("RESUME".toLowerCase());
+        System.out.println(("TITLE: " + title).toLowerCase());
+        System.out.println(("DEGREE: " + degree).toLowerCase());
+        System.out.println(("CAREER: " + career).toLowerCase());
+    }
+}
