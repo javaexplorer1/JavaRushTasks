40ee6065fb0663c449be6290371f2733c134fc5b
Dmitriy <dmed832@yandex.ru>
2022-10-16 22:30:04 +0300

    add solution JavaSyntax from pro.task03.task0316

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0316/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.pro.task03.task0316;
+
+import java.util.Scanner;
+
+/* 
+Орбитальный лифт
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int speed = scanner.nextInt();
+
+        if (speed < 0)
+            System.out.println("System error!");
+        else if (speed > 3) {
+            System.out.println("Скорость лифта слишком высокая, снижаем...");
+            speed = 3;
+        } else {
+            System.out.println("Скорость лифта в норме");
+        }
+    }
+}
