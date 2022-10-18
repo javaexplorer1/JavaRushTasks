77aaeed244c855f4f098ff0538fb448dacd70253
Dmitriy <dmed832@yandex.ru>
2022-10-18 03:39:10 +0300

    add solution JavaSyntax from pro.task03.task0311

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0311/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.pro.task03.task0311;
+
+import java.util.Scanner;
+
+/* 
+Высокая точность
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double number1 = scanner.nextDouble();
+        double number2 = scanner.nextDouble();
+        if (Math.abs(number1 - number2) < 0.000001) {
+            System.out.println("числа равны");
+        }
+        else{
+            System.out.println("числа не равны");
+        }
+    }
+}
