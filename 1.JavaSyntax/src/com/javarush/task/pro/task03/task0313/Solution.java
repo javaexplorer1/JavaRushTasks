bc4ec521b62c1371224aee401d24a13d31dc1c2e
Dmitriy <dmed832@yandex.ru>
2022-10-19 23:54:57 +0300

    add solution JavaSyntax from pro.task03.task0313

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0313/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.pro.task03.task0313;
+
+public class Solution {
+    public static String string1 = "Амиго";
+    public static String string2 = string1;
+    public static String string3 = new String(string1);
+
+    public static void main(String[] args) {
+        String same = "ссылки на строки одинаковые";
+        String different = "ссылки на строки разные";
+        //напишите тут ваш код
+        System.out.println(same);
+        System.out.println(different);
+    }
+}
