aa1f28ba038d8549a73d87d3af4aa46d9fd133cc
Dmitriy <dmed832@yandex.ru>
2022-10-21 00:17:35 +0300

    add solution JavaSyntax from pro.task04.task0412

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0412/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.pro.task04.task0412;
+
+import java.util.Scanner;
+
+/* 
+Сумма кратных чисел
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int start = scanner.nextInt();
+        int end = scanner.nextInt();
+        int multiple = scanner.nextInt();
+
+        int sum = 0;
+        for (int i = start; i < end; i++) {
+            if (i % multiple != 0) {
+                continue;
+            }
+            sum = sum + i;
+        }
+        System.out.println(sum);
+    }
+}
