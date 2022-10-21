0cc5360a1216f416876293ede21ef818987a545a
Dmitriy <dmed832@yandex.ru>
2022-10-22 01:42:29 +0300

    add solution JavaSyntax from pro.task05.task0508

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0508/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.task.pro.task05.task0508;
+
+import java.util.Scanner;
+
+/* 
+Удаляем одинаковые строки
+*/
+
+public class Solution {
+    public static String[] strings;
+
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        strings = new String[6];
+        for (int i = 0; i < strings.length; i++) {
+            strings[i] = console.nextLine();
+        }
+        for (int i = 0; i < strings.length; i++) {
+            String str = strings[i];
+            for (int j = i + 1; j < strings.length; j++) {
+                if (str == null) {
+                    break;
+                }
+                if (str.equals(strings[j])) {
+                    strings[i] = null;
+                    strings[j] = null;
+                }
+            }
+        }
+
+        for (int i = 0; i < strings.length; i++) {
+            System.out.print(strings[i] + ", ");
+        }
+    }
+}
