a124beca7438a69d49444e2a95831d7abe09a2f2
Dmitriy <dmed832@yandex.ru>
2022-10-25 00:25:39 +0300

    add solution JavaSyntax from pro.task04.task0425 (Digger3)

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0425/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0425/Solution.java
@@ -15,13 +15,15 @@ public class Solution {
     public static void main(String[] args) {
         for (int i = 1; i <= 10 ; i++) {
             for (int j = 1; j <= 10 ; j++) {
-                if (i == 1 && j == 4) {
-                    System.out.print(robot);
+                if (i <= 8 && j == 4) {
+                    System.out.print(hole);
                 }
-                else if (i == 10) {
-                        System.out.print(bug);
+                else if (i == 9 && j == 4) {
+                        System.out.print(robot);
                     }
-                    else {
+                    else if (i == 10) {
+                    System.out.print(bug);
+                    } else {
                     System.out.print(land);
                     }
             }
