3fed708e61c4910071dead2023be9ae917571756
Dmitriy <dmed832@yandex.ru>
2022-10-25 00:01:01 +0300

    add solution JavaSyntax from pro.task04.task0425

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0425/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0425/Solution.java
@@ -14,10 +14,16 @@ public class Solution {
 
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
-            for (int j = 1; j <= 10 ; j++) {
-                System.out.print(land);
+            for (int j = 1; j <= 10; j++) {
+                if (i == 1 && j == 4) {
+                    System.out.print(robot);
+                } else if (i == 10) {
+                    System.out.print(bug);
+                } else {
+                    System.out.print(land);
+                }
             }
             System.out.println();
-            }
         }
+    }
 }
