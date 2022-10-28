b9112a248d7e11d8d65b5201646b11ff38759b7f
Dmitriy <dmed832@yandex.ru>
2022-10-28 23:49:55 +0300

    add solution JavaSyntax from pro.task07.task0720 (3/6)

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0720/BattleField.java
@@ -0,0 +1,45 @@
+package com.javarush.task.pro.task07.task0720;
+
+/* 
+Галавир XIII (3)
+*/
+
+public class BattleField {
+    public static String nimrodDefence = "Вражеская атака отбита";
+    public static String nimrodAttack = "Наносим контр удар";
+    public static String nimrodDestroy = "Вражеский удар не выдержан";
+    public static String galavirDefence = "Наша атака успешно отражена врагом";
+    public static String galavirAttack = "Враг наносит удар";
+    public static String galavirDestroy = "Вражеский корабль повержен";
+
+    public static void main(String[] args) {
+        GalavirXIII galavir = new GalavirXIII();
+        Nimrod nimrod = new Nimrod();
+        do {
+            System.out.println(galavirAttack);
+            int attackGalavir = galavir.attack();
+            nimrod.defend(attackGalavir);
+            System.out.println("Nimrod health = " + nimrod.health);
+        } while (isNimrodAlive(nimrod));
+    }
+
+    public static boolean isNimrodAlive(Nimrod nimrod) {
+        if (nimrod.health > 0) {
+            System.out.println(nimrodDefence);
+            return true;
+        } else {
+            System.out.println(nimrodDestroy);
+            return false;
+        }
+    }
+
+    public static boolean isGalavirAlive(GalavirXIII galavir) {
+        if (galavir.health > 0) {
+            System.out.println(galavirDefence);
+            return true;
+        } else {
+            System.out.println(galavirDestroy);
+            return false;
+        }
+    }
+}
