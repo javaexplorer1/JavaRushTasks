2be837e9c87edb78b39f8c0257db5a5f968f79a1
Dmitriy <dmed832@yandex.ru>
2022-11-04 17:45:41 +0300

    add solution JavaSyntax from pro.task06.task0605

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0605/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.task.pro.task06.task0605;
+
+/* 
+Правильный порядок
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int[] array = {11, 22, 33, 44, 55, 66, 77, 88, 99};
+        printArray(array);
+        reverseArray(array);
+        printArray(array);
+    }
+
+    public static void reverseArray(int[] array) {
+
+        int j = array.length;
+        int temp;
+        for (int i = 0; i < (array.length / 2); i++) {
+            j--;
+            temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+
+    public static void printArray(int[] array) {
+        for (int i : array) {
+            System.out.print(i + ", ");
+        }
+        System.out.println();
+    }
+}
