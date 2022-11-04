5f227ea26be96fa30bed358760d229ba40c06cf1
Dmitriy <dmed832@yandex.ru>
2022-11-04 22:25:52 +0300

    add solution JavaSyntax from pro.task07.task0705

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0705/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task07.task0705;
+
+/* 
+Бесконечность не предел
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        div(0, 1);
+        div(0, -1);
+
+    }
+
+    public static void div(double a, double b){
+        System.out.println(b/a);
+    }
+}
