2b2f0cdb06685736a1260c4961084459047d6599
Dmitriy <dmed832@yandex.ru>
2022-11-04 21:47:45 +0300

    add solution JavaSyntax from pro.task07.task0702

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0702/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task07.task0702;
+
+/* 
+Максимальное целое
+*/
+
+public class Solution {
+    byte b = 127;
+    short sh = 32767;
+    int i = 2147483647;
+    long l = Long.MAX_VALUE;
+
+}
