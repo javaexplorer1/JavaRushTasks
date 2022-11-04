ef2c0e9358c8578a00498521d1f4e2b2d5c56d1e
Dmitriy <dmed832@yandex.ru>
2022-11-04 22:27:29 +0300

    add solution JavaSyntax from pro.task07.task0706

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0706/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.pro.task07.task0706;
+
+/* 
+Странное деление
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        div(0, 0);
+    }
+
+    public static void div(double a, double b){
+        System.out.println(b/a);
+    }
+}
