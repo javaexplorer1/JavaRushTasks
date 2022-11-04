a16593d828e80de9d7d23d0b2f37aab475fd6d8a
Dmitriy <dmed832@yandex.ru>
2022-11-04 22:19:12 +0300

    add solution JavaSyntax from pro.task07.task0703

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0703/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task07.task0703;
+
+/* 
+Минимальное целое
+*/
+
+public class Solution {
+    byte b = -128;
+    short s = -32768;
+    int i = Integer.MIN_VALUE;
+    long l = Long.MIN_VALUE;
+}
