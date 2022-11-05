a2a9d7348243592300c95e056b5e038df71c2ad9
Dmitriy <dmed832@yandex.ru>
2022-11-05 03:25:38 +0300

    add solution JavaSyntax from pro.task08.task0801

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0801/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.pro.task08.task0801;
+
+import static java.lang.Math.toRadians;
+
+/* 
+Утильный класс: часть 1
+*/
+
+public class Solution {
+
+    public static double sin(double a) {
+        double rad = Math.toRadians(a);
+        return Math.sin(rad);
+    }
+
+    public static double cos(double a) {
+        double rad = Math.toRadians(a);
+        return Math.cos(rad);
+    }
+
+    public static double tan(double a) {
+        double rad = Math.toRadians(a);
+        return Math.tan(rad);
+    }
+}
