93b5b347f7bb6cb8b8172c17ba9bf68001f4193d
Dmitriy <dmed832@yandex.ru>
2022-11-05 03:08:24 +0300

    add solution JavaSyntax from pro.task07.task0712

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0712/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.pro.task07.task0712;
+
+/* 
+Первые объекты
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String string1 = new String();
+        String string2 = new String();
+        String string3 = new String();
+        String string4 = new String();
+        String string5 = new String();
+        String string6 = new String();
+        String string7 = new String();
+        String string8 = new String();
+        String string9 = new String();
+        String string10 = new String();
+        int[] int1 = new int[10];
+        int[] int2 = new int[10];
+        int[] int3 = new int[10];
+        int[] int4 = new int[10];
+        int[] int5 = new int[10];
+        Scanner console1 = new Scanner(System.in);
+        Scanner console2 = new Scanner(System.in);
+    }
+}
