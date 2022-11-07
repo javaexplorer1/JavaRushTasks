0b8acd06336cd2be1d320c881a7189761cb0456c
Dmitriy <dmed832@yandex.ru>
2022-11-07 23:46:12 +0300

    add solution JavaSyntax from pro.task09.task0905

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0905/Solution.java
@@ -0,0 +1,44 @@
+package com.javarush.task.pro.task09.task0905;
+
+import java.util.regex.Pattern;
+
+/* 
+Восьмеричный конвертер
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int decimalNumber = 21;
+        System.out.println("Десятичное число " + decimalNumber + " равно восьмеричному числу " + toOctal(decimalNumber));
+        int octalNumber = 25;
+        System.out.println("Восьмеричное число " + octalNumber + " равно десятичному числу " + toDecimal(octalNumber));
+    }
+
+    public static int toOctal(int decimalNumber) {
+        if (decimalNumber <= 0) {
+            return 0;
+        }
+        int i = 0;
+        int octalNumber = 0;
+        while (decimalNumber != 0) {
+            octalNumber = octalNumber + (decimalNumber % 8) * (int) Math.pow(10, i);
+            decimalNumber = decimalNumber / 8;
+            i++;
+        }
+        return octalNumber;
+    }
+
+    public static int toDecimal(int octalNumber) {
+        if (octalNumber <= 0) {
+            return 0;
+        }
+        int i = 0;
+        int decimalNumber = 0;
+        while (octalNumber != 0) {
+            decimalNumber = decimalNumber + (octalNumber % 10) * (int) Math.pow(8, i);
+            octalNumber = octalNumber / 10;
+            i++;
+        }
+        return decimalNumber;
+    }
+}
