9c5e0366fb3850d8f8b196d415e8a861d77e070f
Dmitriy <dmed832@yandex.ru>
2022-11-07 23:47:14 +0300

    add solution JavaSyntax from pro.task09.task0907

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0907/Solution.java
@@ -0,0 +1,41 @@
+package com.javarush.task.pro.task09.task0907;
+
+import java.util.regex.Pattern;
+
+/* 
+Шестнадцатеричный конвертер
+*/
+
+public class Solution {
+    private static final String HEX = "0123456789abcdef";
+
+    public static void main(String[] args) {
+        int decimalNumber = 1256;
+        System.out.println("Десятичное число " + decimalNumber + " равно шестнадцатеричному числу " + toHex(decimalNumber));
+        String hexNumber = "4e8";
+        System.out.println("Шестнадцатеричное число " + hexNumber + " равно десятичному числу " + toDecimal(hexNumber));
+    }
+
+    public static String toHex(int decimalNumber) {
+        if (decimalNumber <= 0 ) {
+            return "";
+        }
+        String hexNumber = "";
+        while (decimalNumber != 0) {
+            hexNumber = HEX.charAt(decimalNumber % 16) + hexNumber;
+            decimalNumber /= 16;
+        }
+        return hexNumber;
+    }
+
+    public static int toDecimal(String hexNumber) {
+        if (hexNumber == null || hexNumber.isEmpty()) {
+            return 0;
+        }
+        int decimalNumber = 0;
+        for (int i = 0; i < hexNumber.length(); i++) {
+            decimalNumber = 16 * decimalNumber + HEX.indexOf(hexNumber.charAt(i));
+        }
+        return decimalNumber;
+    }
+}
