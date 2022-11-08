331f04167545b7bb99a0084e58c36b50b5d8a6eb
Dmitriy <dmed832@yandex.ru>
2022-11-08 03:54:31 +0300

    add solution JavaSyntax from pro.task08.task0818

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0818/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.task.pro.task08.task0818;
+
+/* 
+Путь через аномалию
+*/
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public static int[][] anomalySpace = new int[][]{
+            {383, 961, 904}, {769, 887, 770}, {184, 608, 406}, {310, 821, 445}, {412, 908, 158},
+            {562, 844, 573}, {260, 880, 230}, {635, 254, 756}, {816, 754, 508}, {947, 195, 379},
+            {576, 678, 682}, {207, 653, 588}, {901, 315, 223}, {310, 561, 255}, {199, 356, 350},
+            {971, 276, 974}, {413, 829, 494}, {344, 345, 322}, {889, 713, 222}, {628, 963, 234},
+            {254, 428, 926}, {633, 762, 848}, {896, 130, 653}, {971, 636, 861}, {328, 976, 139}};
+
+    public static double[] safeRoute = new double[25];
+
+    public static void main(String[] args) {
+        int ind = 0;
+        System.out.println(anomalySpace.length);
+        for (int i = 0; i < anomalySpace.length; i++) {
+            int x = anomalySpace[i][0];
+            int y = anomalySpace[i][1];
+            int z = anomalySpace[i][2];
+            double v = (Math.sin(x) * Math.PI) + (Math.cos(y) * Math.min(x, Math.min(y, z))) + (Math.tan(y) / Math.log(z));
+            if (v >= 100 && v <= 200) {
+                safeRoute[ind] = v;
+                ind++;
+            }
+        }
+        // System.out.println(Arrays.toString(safeRoute));
+    }
+}
