6d8b48c3f165fa3735f0836d351ed80976541e94
Dmitriy <dmed832@yandex.ru>
2022-11-14 00:17:14 +0300

    add solution JavaSyntax from pro.task08.task0813

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0813/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task08.task0813;
+
+/* 
+Степень двойки
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(getPowerOfTwo(3));
+    }
+
+    public static int getPowerOfTwo(int power) {
+        int result = 2;
+        return (result << (power - 1));
+    }
+}
