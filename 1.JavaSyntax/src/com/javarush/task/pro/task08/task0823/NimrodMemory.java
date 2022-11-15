ea928cc20e287df84cccc6b9f2e6b0d8757bfe2b
Dmitriy <dmed832@yandex.ru>
2022-11-15 22:15:50 +0300

    add solution JavaSyntax from pro.task08.task0823

--- 1.JavaSyntax/src/com/javarush/task/pro/task08/task0823/NimrodMemory.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0823/NimrodMemory.java
@@ -4,19 +4,38 @@ package com.javarush.task.pro.task08.task0823;
 Очистка памяти корабля
 */
 
+
+import java.util.Random;
+
 public class NimrodMemory {
 
     public static long[] santaFabrica = new long[99000];
     public static long[] eroticSimulators = new long[100500];
 
     public static void main(String[] args) {
-        // test
+        Random r = new Random();
+        for (int i = 0; i < santaFabrica.length; i++) {
+            santaFabrica[i] = r.nextLong();
+        }
+
+        for (int i = 0; i < eroticSimulators.length; i++) {
+            eroticSimulators[i] = r.nextLong();
+        }
+        System.out.println(Long.toBinaryString(santaFabrica[0]));
         markForDeletion();
         System.out.println(Long.toBinaryString(santaFabrica[0]));
     }
 
     public static void markForDeletion() {
-        //напишите тут ваш код
-
+        for (int i = 0; i < santaFabrica.length; i++) {
+            santaFabrica[i] = santaFabrica[i] | (1 << 13);
+            santaFabrica[i] = santaFabrica[i] | (1 << 21);
+            santaFabrica[i] = santaFabrica[i] | (1 << 29);
+        }
+        for (int i = 0; i < eroticSimulators.length; i++) {
+            eroticSimulators[i] = eroticSimulators[i] | (1 << 13);
+            eroticSimulators[i] = eroticSimulators[i] | (1 << 21);
+            eroticSimulators[i] = eroticSimulators[i] | (1 << 29);
+        }
     }
 }
