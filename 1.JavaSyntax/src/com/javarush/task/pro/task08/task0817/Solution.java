f1c8044a3fe8d000d01ef09836f9e7c23dc9f225
Dmitriy <dmed832@yandex.ru>
2022-11-15 21:52:56 +0300

    add solution JavaSyntax from pro.task08.task0817

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0817/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.pro.task08.task0817;
+
+/* 
+Инкременты
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int a = 3;
+        int b = ++a + (a-- * ++a);
+
+        System.out.println(b);
+    }
+}
