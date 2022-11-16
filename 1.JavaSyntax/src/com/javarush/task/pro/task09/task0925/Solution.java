d3770a9e8f7a3e42905e15f3e1aa45b6d093924c
Dmitriy <dmed832@yandex.ru>
2022-11-16 03:20:05 +0300

    add solution JavaSyntax from pro.task09.task0925 (condition)

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0925/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.pro.task09.task0925;
+
+/* 
+Освобождение Пиратов
+*/
+
+import java.util.StringTokenizer;
+
+import java.util.StringTokenizer;
+
+public class Solution {
+
+    public static String[] controlUnits = {"iAm/A/spyware/one", "iAm/A/malware/two", "iAm/A/virus/three", "iAm/A/trojan/four", "iAm/bug/five"};
+
+    public static void main(String[] args) {
+        splitArrayElements();
+        getStringFromArray();
+        printArray(controlUnits);
+    }
+
+    public static void splitArrayElements() {
+        //напишите тут ваш код
+
+    }
+
+    public static void getStringFromArray() {
+        //напишите тут ваш код
+
+    }
+
+    public static void printArray(String[] array) {
+        for (String s : array) {
+            System.out.println(s);
+        }
+    }
+}
