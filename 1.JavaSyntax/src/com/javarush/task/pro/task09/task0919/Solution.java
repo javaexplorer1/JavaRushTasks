3a0de5e4db1e8db56dd591792b0a9ec98a418e0e
Dmitriy <dmed832@yandex.ru>
2022-11-19 19:04:25 +0300

    add solution JavaSyntax from pro.task09.task0919

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0919/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task09.task0919;
+
+/* 
+Разворот строки
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String string = "Ходит кот задом наперед";
+        System.out.println(string);
+        System.out.println(reverseString(string));
+    }
+
+    public static String reverseString(String string) {
+        StringBuilder reverseString = new StringBuilder(string);
+        return reverseString.reverse().toString();
+    }
+}
