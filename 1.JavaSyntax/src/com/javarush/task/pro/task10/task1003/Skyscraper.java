d04d537a1110dbb0b0c0f2e8ba7a2d0f9fd93ef1
Dmitriy <dmed832@yandex.ru>
2022-11-19 21:40:22 +0300

    add solution JavaSyntax from pro.task10.task1003

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1003/Skyscraper.java
@@ -0,0 +1,25 @@
+package com.javarush.task.pro.task10.task1003;
+
+/* 
+Построим новый бизнес-комплекс JavaRush Business Center
+*/
+
+public class Skyscraper {
+    private int floorsCount;
+    private String developer;
+
+    public Skyscraper () {
+        floorsCount = 5;
+        developer = "JavaRushDevelopment";
+    }
+
+    public Skyscraper (int floorsCount, String developer) {
+        this.floorsCount = floorsCount;
+        this.developer = developer;
+    }
+
+    public static void main(String[] args) {
+        Skyscraper skyscraper = new Skyscraper();
+        Skyscraper skyscraperUnknown = new Skyscraper(50, "Неизвестно");
+    }
+}
