fb8290858e42e4a2b95dbfb3dfc9f5774ff7dda1
Dmitriy <dmed832@yandex.ru>
2022-11-20 23:57:23 +0300

    add solution JavaSyntax from pro.task10.task1020

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1020/Computer.java
@@ -0,0 +1,11 @@
+package com.javarush.task.pro.task10.task1020;
+
+public class Computer {
+
+    public Computer () {
+        new Monitor();
+        new Mouse();
+        new Keyboard();
+        new SystemUnit();
+    }
+}
