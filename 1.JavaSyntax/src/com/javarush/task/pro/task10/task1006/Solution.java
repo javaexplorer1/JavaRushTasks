532a82a152bc428a3e3709cd5da855456b8da342
Dmitriy <dmed832@yandex.ru>
2022-11-22 21:03:00 +0300

    add solution JavaSyntax from pro.task10.task1006

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1006/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task10.task1006;
+
+/* 
+Кто тут наследник?
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        ElectricCar electricCar = new ElectricCar();
+        GasCar gasCar = new GasCar();
+        HybridCar hybridCar = new HybridCar();
+
+    }
+}
