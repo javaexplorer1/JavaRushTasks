1062b508df527afa96ad3256c1ad82030c10e1f4
Dmitriy <dmed832@yandex.ru>
2022-11-23 03:13:14 +0300

    add solution JavaSyntax from pro.task10.task1010

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1010/Iphone.java
@@ -0,0 +1,43 @@
+package com.javarush.task.pro.task10.task1010;
+
+import java.util.Objects;
+
+/* 
+Два айфона
+*/
+
+public class Iphone {
+    private String model;
+    private String color;
+    private int price;
+
+    public Iphone(String model, String color, int price) {
+        this.model = model;
+        this.color = color;
+        this.price = price;
+    }
+
+    public boolean equals(Object obj) {
+
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof Iphone)) {
+            return false;
+        }
+        Iphone iphone = (Iphone) obj;
+       return price == iphone.price && Objects.equals(model, iphone.model) && Objects.equals(color, iphone.color);
+    }
+
+
+    public static void main(String[] args) {
+        Iphone iphone1 = new Iphone("X", "Black", 999);
+        Iphone iphone2 = new Iphone("X", "Black", 999);
+
+        System.out.println(iphone1.equals(iphone2));
+    }
+
+}
