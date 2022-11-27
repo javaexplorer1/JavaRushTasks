589f52d713a38f490e14aa23647ebf820ecfd768
Dmitriy <dmed832@yandex.ru>
2022-11-27 19:56:06 +0300

    add solution JavaSyntax from pro.task12.task1210

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1210/Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.task.pro.task12.task1210;
+
+import java.util.ArrayList;
+
+/* 
+Переворачивание данных
+*/
+
+public class Solution {
+    public static ArrayList<Integer> numbers = new ArrayList<>();
+
+    public static void main(String[] args) {
+        init();
+        print();
+
+        reverse();
+        print();
+    }
+
+    public static void init() {
+        for (int i = 0; i < 10; i++) {
+            numbers.add(i);
+        }
+    }
+
+    public static void reverse() {
+        int n = numbers.size()-1;
+        for (int i = 0; i < numbers.size() / 2; i++) {
+            int temp = numbers.get(i);
+            numbers.set(i, numbers.get(n - i));
+            numbers.set(n - i, temp);
+        }
+    }
+
+    private static void print() {
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+    }
+}
