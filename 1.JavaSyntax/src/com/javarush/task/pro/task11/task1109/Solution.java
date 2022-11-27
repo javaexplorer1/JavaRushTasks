b544e27de8bcc763d976cb6cb1fd9f284078f2fe
Dmitriy <dmed832@yandex.ru>
2022-11-27 23:41:26 +0300

    add solution JavaSyntax from pro.task11.task1109

--- 1.JavaSyntax/src/com/javarush/task/pro/task11/task1109/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1109/Solution.java
@@ -6,9 +6,7 @@ package com.javarush.task.pro.task11.task1109;
 
 public class Solution {
     public static void main(String[] args) {
-
         Outer.Inner inner = new Outer().new Inner();
         Outer.Nested nested = new Outer.Nested();
-
     }
 }
