8eebe9274f8d53e6393d2b2c44acdb96ef6b4265
Dmitriy <dmed832@yandex.ru>
2022-11-28 21:48:27 +0300

    add solution JavaSyntax from pro.task11.task1122

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1122/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task11.task1122;
+
+/* 
+Затенение поля класса
+*/
+
+public class Solution {
+    public static int salary;
+
+    public static void add(int increase) {
+        salary += increase;
+    }
+    public static void main(String[] args) {
+        add(100500);
+        System.out.println(salary);
+    }
+}
