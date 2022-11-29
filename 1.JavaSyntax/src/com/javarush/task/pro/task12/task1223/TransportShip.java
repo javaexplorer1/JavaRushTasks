0dd0828f57473f866367173cbce39502c12c5c26
Dmitriy <dmed832@yandex.ru>
2022-11-30 01:35:47 +0300

    add solution JavaSyntax from pro.task12.task1223

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1223/TransportShip.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task12.task1223;
+
+public class TransportShip {
+    public Integer speed;
+    public Integer acceleration;
+    public Passenger[] passengers;
+    public OnBoardSystem onBoardSystem = new OnBoardSystem();
+
+    public class OnBoardSystem {
+        public Integer calculateJumpTime(Integer distance) {
+            int time = 0;
+            int way = 0;
+            while (way < distance) {
+                time++;
+                way = (speed * time) + (acceleration * time * time / 2);
+            }
+            return time - 1;
+        }
+    }
+}
