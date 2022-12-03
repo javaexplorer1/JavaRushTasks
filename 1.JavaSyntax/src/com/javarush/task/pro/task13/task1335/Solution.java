699dcc1ec13155077617802cd3aa779aba4d0df6
Dmitriy <dmed832@yandex.ru>
2022-12-04 02:35:34 +0300

    add solution JavaSyntax from pro.task13.task1335

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1335/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.pro.task13.task1335;
+
+/* 
+Дорога к выходу
+*/
+
+import java.util.List;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        printList(clearBoard());
+
+    }
+
+    public static List<Board.Node> clearBoard() {
+        Board board = new Board();
+        List<Board.Node> boardNodes = board.getBoardNodes();
+        for (Board.Node boardNode : boardNodes) {
+            if (!boardNode.getData().equals("board")) {
+                boardNode.setData("board");
+            }
+        }
+        return boardNodes;
+    }
+
+    private static void printList(List<?> list) {
+        list.forEach(element -> System.out.print(element + " "));
+    }
+}
