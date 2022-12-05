2dca8b245ee1241e524c400686261a54ad917a07
Dmitriy <dmed832@yandex.ru>
2022-12-05 23:43:27 +0300

    add solution JavaSyntax from pro.task12.task1227

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1227/Solution.java
@@ -0,0 +1,50 @@
+package com.javarush.task.pro.task12.task1227;
+
+/* 
+   Ох уж эта жуткая корзина
+*/
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+
+    public static List<Robot> cableContents = new ArrayList<>(Arrays.asList(new Robot("T1000"), new Robot("Amigo"),
+            new Robot("B1"), new Robot("Diego"), new Robot("Zed")));
+    private static List<Robot> rescuedRobots = new ArrayList<>();
+
+    public static void main(String[] args) {
+        rescueRobots();
+        emptyGarbageBin();
+        printList(rescuedRobots);
+    }
+
+    public static void rescueRobots() {
+        for (int i = 0; i < cableContents.size(); i++) {
+            Robot robot = cableContents.get(i);
+            if (robot.getName().equals("Amigo")) {
+                rescuedRobots.add(robot);
+                cableContents.remove(robot);
+                i--;
+            }
+            if (robot.getName().equals("Diego")) {
+                rescuedRobots.add(robot);
+                cableContents.remove(robot);
+                i--;
+            }
+        }
+    }
+
+    private static void emptyGarbageBin() {
+        cableContents.removeAll(rescuedRobots);
+        System.out.println("Garbage cleared");
+    }
+
+    public static void printList(List<Robot> list) {
+        for (Robot o : list) {
+            System.out.print(o + " ");
+        }
+        System.out.println();
+    }
+}
