66cec3c0623372e2910e4163ef915d6f59049d9c
Dmitriy <dmed832@yandex.ru>
2022-12-06 00:00:50 +0300

    add solution JavaSyntax from pro.task12.task1220

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1220/Solution.java
@@ -0,0 +1,51 @@
+package com.javarush.task.pro.task12.task1220;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+/* 
+Охотники за привидением: в погоне за типами
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        var elements = new ArrayList<>();
+        elements.add("Привет");
+        elements.add(10);
+        elements.add(new Integer[15]);
+        elements.add(new LinkageError());
+
+        checkElementsType(elements);
+    }
+
+    public static void checkElementsType(ArrayList<Object> elements) {
+        for (Object element : elements) {
+            if (element instanceof String) {
+                printString();
+            } else if (element instanceof Integer) {
+                printInteger();
+            } else if (element instanceof Integer[]) {
+                printIntegerArray();
+            } else {
+                printUnknown();
+            }
+        }
+    }
+
+    public static void printString() {
+        System.out.println("Строка");
+    }
+
+    public static void printInteger() {
+        System.out.println("Целое число");
+    }
+
+    public static void printIntegerArray() {
+        System.out.println("Массив целых чисел");
+    }
+
+    public static void printUnknown() {
+        System.out.println("Другой тип данных");
+    }
+}
