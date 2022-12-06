4ddde89a206bd565bd119e43529edcbd8fdb81c5
Dmitriy <dmed832@yandex.ru>
2022-12-06 21:18:43 +0300

    add solution JavaSyntax from pro.task13.task1305

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1305/Solution.java
@@ -0,0 +1,67 @@
+package com.javarush.task.pro.task13.task1305;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/* 
+Найти и обезвредить
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        ArrayList<String> words = new ArrayList<>();
+        words.add("Hello world!");
+        words.add("Amigo");
+        words.add("Elly");
+        words.add("Kerry");
+        words.add("Bug");
+        words.add("bug");
+        words.add("Easy ug");
+        words.add("Risha");
+
+        ArrayList<String> copyWordsFirst = new ArrayList<>(words);
+        ArrayList<String> copyWordsSecond = new ArrayList<>(words);
+        ArrayList<String> copyWordsThird = new ArrayList<>(words);
+
+        removeBugWithFor(copyWordsFirst);
+        removeBugWithWhile(copyWordsSecond);
+        removeBugWithCopy(copyWordsThird);
+
+        copyWordsFirst.forEach(System.out::println);
+        String line = "_________________________";
+        System.out.println(line);
+        copyWordsSecond.forEach(System.out::println);
+        System.out.println(line);
+        copyWordsThird.forEach(System.out::println);
+        System.out.println(line);
+    }
+
+    public static void removeBugWithFor(ArrayList<String> list) {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).toLowerCase().equals("bug")) {
+                list.remove(i);
+                i--;
+            }
+        }
+    }
+
+    public static void removeBugWithWhile(ArrayList<String> list) {
+        Iterator<String> it = list.iterator();
+        while (it.hasNext()) {
+            String str = it.next();
+            if (str.toLowerCase().equals("bug")) {
+                it.remove();
+            }
+        }
+    }
+
+    public static void removeBugWithCopy(ArrayList<String> list) {
+        ArrayList<String> listCopy = new ArrayList<>(list);
+        for (String str : listCopy) {
+            if (str.toLowerCase().equals("bug")) {
+                list.remove(str);
+            }
+        }
+    }
+}
