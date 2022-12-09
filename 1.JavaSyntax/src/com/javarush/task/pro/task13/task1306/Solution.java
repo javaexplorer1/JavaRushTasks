33652aa092e0c0ecc9c54a1f179e0c94cb9e81b3
Dmitriy <dmed832@yandex.ru>
2022-12-09 23:20:20 +0300

    add solution JavaSyntax from pro.task13.task1306

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1306/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.pro.task13.task1306;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/* 
+Изучаем методы класса Collections, часть 1
+*/
+
+public class Solution {
+
+    public static void copy(ArrayList<String> destination, ArrayList<String> source) {
+        if(destination.size() < source.size()) {
+            throw new IndexOutOfBoundsException("Source does not fit in dest");
+        }
+        Collections.copy(destination, source);
+        }
+
+    public static void addAll(ArrayList<String> list, String... strings) {
+        Collections.addAll(list, strings);
+    }
+
+    public static void replaceAll(ArrayList<String> list, String oldValue, String newValue) {
+        Collections.replaceAll(list, oldValue, newValue);
+    }
+}
