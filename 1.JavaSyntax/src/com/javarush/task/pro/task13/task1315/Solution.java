83e4c9a1e9e441f6b2319a00538daec0e812390d
Dmitriy <dmed832@yandex.ru>
2022-12-10 20:21:24 +0300

    add solution JavaSyntax from pro.task13.task1315

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1315/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.pro.task13.task1315;
+
+/* 
+Четыре сезона
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(Season.WINTER);
+        System.out.println(Season.SPRING);
+        System.out.println(Season.SUMMER);
+        System.out.println(Season.AUTUMN);
+    }
+}
