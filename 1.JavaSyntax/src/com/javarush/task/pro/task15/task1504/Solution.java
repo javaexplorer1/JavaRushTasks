0d34efacb93675cfea5e439ca093b7e42dffaa43
Dmitriy <dmed832@yandex.ru>
2022-12-25 23:55:50 +0300

    add solution JavaSyntax from pro.task15.task1504

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1504/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.pro.task15.task1504;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+/* 
+Перепутанные байты
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+
+        try (Scanner scanner = new Scanner(System.in);
+             InputStream inputStream = Files.newInputStream(Paths.get(scanner.nextLine()));
+             OutputStream outputStream = Files.newOutputStream(Paths.get(scanner.nextLine()))) {
+            byte[] bytesIn = inputStream.readAllBytes();
+            byte[] bytesOut = new byte[bytesIn.length];
+            for (int i = 0; i < bytesIn.length; i += 2) {
+                if (i < bytesIn.length - 1) {
+                    bytesOut[i] = bytesIn[i + 1];
+                    bytesOut[i + 1] = bytesIn[i];
+                } else {
+                    bytesOut[i] = bytesIn[i];
+                }
+            }
+            outputStream.write(bytesOut);
+        }
+
+
+    }
+}
+
