ff9f596cc5502b8d1d9081773ecfcc4ed953f644
Dmitriy <dmed832@yandex.ru>
2022-12-29 23:37:25 +0300

    add solution JavaSyntax from pro.task15.task1515

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1515/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.pro.task15.task1515;
+
+import java.nio.file.Path;
+import java.util.Scanner;
+
+/* 
+Абсолютный путь
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        Path path = Path.of(str);
+        if (path.isAbsolute()) {
+            System.out.println(str);
+        } else {
+            System.out.println(path.toAbsolutePath());
+        }
+    }
+}
+
