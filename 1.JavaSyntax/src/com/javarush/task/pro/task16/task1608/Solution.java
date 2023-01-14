b70eac6a694f8e1f2bf0d6a879dcd29f14da08b9
Dmitriy <dmed832@yandex.ru>
2023-01-14 23:31:54 +0300

    add solution JavaSyntax from pro.task16.task1608

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task16/task1608/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.pro.task16.task1608;
+
+import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
+/* 
+Просто день недели
+*/
+
+public class Solution {
+
+    static LocalDate birthDate = LocalDate.of(2020, 3, 12);
+
+    public static void main(String[] args) {
+        System.out.println(getDayOfWeek(birthDate));
+    }
+
+    static String getDayOfWeek(LocalDate date) {
+        return date.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.forLanguageTag("ru"));
+    }
+}
