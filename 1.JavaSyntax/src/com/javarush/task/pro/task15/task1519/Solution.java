5ac26e1f20c72a45157bf417710c2939ab4c5d0a
Dmitriy <dmed832@yandex.ru>
2023-01-17 03:02:49 +0300

    add solution JavaSyntax from pro.task15.task1519

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1519/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.pro.task15.task1519;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+
+/* 
+Поверхностное копирование
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        Path sourceDirectory = Path.of(scanner.nextLine());
+        Path targetDirectory = Path.of(scanner.nextLine());
+        if (Files.isDirectory(sourceDirectory) && Files.isDirectory(targetDirectory)) {
+            try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(sourceDirectory)) {
+                for (Path path : directoryStream) {
+                    if (Files.isRegularFile(path)) {
+                        Files.copy(path, targetDirectory.resolve(path.getFileName()));
+                    }
+                }
+            }
+        }
+    }
+}
+
