347244a9c78de8c447adc9737ad5f035d0900777
Dmitriy <dmed832@yandex.ru>
2023-01-17 03:10:19 +0300

    add solution JavaSyntax from pro.task11.task1123

---
+++ 2.JavaCore/src/com/javarush/task/task11/task1123/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.task.task11.task1123;
+
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] data = new int[]{1, 2, 3, 5, -2, -8, 0, 77, 5, 5};
+
+        Pair result = getMinimumMaximumPair(data);
+
+        System.out.println("The minimum is " + result.x);
+        System.out.println("The maximum is " + result.y);
+    }
+
+    public static Pair getMinimumMaximumPair(int[] inputArray) {
+        if (inputArray == null || inputArray.length == 0) {
+            return new Pair(null, null);
+        }
+
+        int[] copy = Arrays.copyOf(inputArray, inputArray.length);
+        Arrays.sort(copy);
+        return new Pair(copy[0], copy[copy.length-1]);
+    }
+
+    public static class Pair {
+        public Integer x;
+        public Integer y;
+
+        public Pair(Integer x, Integer y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
