82f5f1ddd047d19155432f7af43ae868c5d831f6
Dmitriy <dmed832@yandex.ru>
2023-01-21 23:18:52 +0300

    add solution JavaSyntax from pro.task17.task1715

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task17/task1715/Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.task.pro.task17.task1715;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Вилларибо и Виллабаджо.
+*/
+
+public class Solution {
+    static List<MusicalInstrument> orchestra = new ArrayList<>();
+
+    public static void main(String[] args) {
+        createKeyboardOrchestra();
+        createStringedOrchestra();
+        playOrchestra();
+    }
+
+    public static void createKeyboardOrchestra() {
+        orchestra.add(new Organ());
+        orchestra.add(new Piano());
+        orchestra.add(new Piano());
+        orchestra.add(new Piano());
+
+    }
+
+    public static void createStringedOrchestra() {
+        orchestra.add(new Violin());
+        orchestra.add(new Violin());
+        orchestra.add(new Guitar());
+    }
+
+    public static void playOrchestra() {
+        for (MusicalInstrument musicalInstrument : orchestra) {
+            musicalInstrument.play();
+        }
+    }
+}
