cb8a5f4a1b7dbf9ab5e47b543a54cae9ac5474f5
Dmitriy <dmed832@yandex.ru>
2023-01-21 21:39:02 +0300

    add solution JavaSyntax from pro.task17.task1701

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task17/task1701/Bat.java
@@ -0,0 +1,11 @@
+package com.javarush.task.pro.task17.task1701;
+
+/* 
+Правильное движение
+*/
+
+public class Bat extends Animal {
+    public void move(){
+        System.out.println("Я лечу!");
+    }
+}
