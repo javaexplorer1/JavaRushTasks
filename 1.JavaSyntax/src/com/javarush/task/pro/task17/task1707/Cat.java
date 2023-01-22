2940697130c141d094d482c3cdf2eaec3400315c
Dmitriy <dmed832@yandex.ru>
2023-01-22 13:29:21 +0300

    add solution JavaSyntax from pro.task17.task1707

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task17/task1707/Cat.java
@@ -0,0 +1,10 @@
+package com.javarush.task.pro.task17.task1707;
+
+public class Cat extends Pet {
+    public static final String CAT = "Я не люблю людей.";
+
+    public void printInfo(){
+        super.printInfo();
+        System.out.println(CAT);
+    }
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task17/task1707/Dog.java
@@ -0,0 +1,10 @@
+package com.javarush.task.pro.task17.task1707;
+
+public class Dog extends Pet {
+    public static final String DOG = "Я люблю людей.";
+
+    public void printInfo(){
+        super.printInfo();
+        System.out.println(DOG);
+    }
+}
