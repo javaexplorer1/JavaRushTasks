17f2c228008e3ab70be6ee495433b8404ef855c1
Dmitriy <dmed832@yandex.ru>
2023-01-24 00:28:25 +0300

    add solution JavaCore from task.task12.task1210

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1210/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task12.task1210;
+
+/* 
+Три метода и максимум
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static int max(int a, int b) {
+        return a > b ? a : b;
+    }
+
+    public static long max(long a, long b) {
+        return a > b ? a : b;
+    }
+
+    public static double max(double a, double b) {
+        return a > b ? a : b;
+    }
+}
