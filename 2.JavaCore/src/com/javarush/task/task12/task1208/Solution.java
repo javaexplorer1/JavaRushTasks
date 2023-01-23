e3f672966a04e1c260a112703cae456843a6de74
Dmitriy <dmed832@yandex.ru>
2023-01-24 00:18:29 +0300

    add solution JavaCore from task.task12.task1208

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    static void print(int a) {
+    }
+
+    static void print(Integer a) {
+    }
+
+    static void print(long a) {
+    }
+
+    static void print(String a) {
+    }
+
+    static void print(int a, int b) {
+    }
+
+
+}
