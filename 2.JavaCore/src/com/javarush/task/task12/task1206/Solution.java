ed932ca2b924eb6d2e8a0ce7d2e09062d572e095
Dmitriy <dmed832@yandex.ru>
2023-01-24 00:07:08 +0300

    add solution JavaCore from task.task12.task1206

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1206/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task12.task1206;
+
+/* 
+Выполняем перегрузку!
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    static void print(int parameter) {
+
+    }
+
+    static void print(String param) {
+
+    }
+}
