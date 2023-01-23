8bd3ec3290150af2f8af4f8e8443c18c550f0b69
Dmitriy <dmed832@yandex.ru>
2023-01-24 00:16:32 +0300

    add solution JavaCore from task.task12.task1207

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static Integer param = 6;
+    public static void main(String[] args) {
+        print(5);
+        print(param);
+    }
+
+    static void print(int param) {
+
+    }
+
+    static void print(Integer param) {
+
+    }
+}
