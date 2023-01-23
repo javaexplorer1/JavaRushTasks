47012e982ffdc9ed95c021badeff6633c9918765
Dmitriy <dmed832@yandex.ru>
2023-01-24 00:23:29 +0300

    add solution JavaCore from task.task12.task1209

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1209/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task12.task1209;
+
+/* 
+Три метода и минимум
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static int min(int a, int b) {
+        return a < b ? a : b;
+    }
+
+    public static long min(long a, long b) {
+        return a < b ? a : b;
+    }
+
+    public static double min(double a, double b) {
+        return a < b ? a : b;
+    }
+
+}
