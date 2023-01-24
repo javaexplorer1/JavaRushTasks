47342c5c286a495c6d7b04dd756fe65d2dc25c3d
Dmitriy <dmed832@yandex.ru>
2023-01-24 23:45:50 +0300

    add solution JavaCore from task.task12.task1220

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1220/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task12.task1220;
+
+/* 
+Класс Human и интерфейсы CanRun, CanSwim
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+
+    public interface CanRun {
+        void run();
+    }
+
+    public interface CanSwim {
+        void swim();
+    }
+
+    public abstract class Human implements CanRun, CanSwim {
+
+    }
+    //add public interfaces and a public class here - добавь public интерфейсы и public класс тут
+}
