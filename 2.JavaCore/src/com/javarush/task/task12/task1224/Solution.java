b5be7e0e5c49d7f9c871c8b3331e3b60cbf74758
Dmitriy <dmed832@yandex.ru>
2023-01-25 00:00:06 +0300

    add solution JavaCore from task.task12.task1224

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1224/Solution.java
@@ -0,0 +1,46 @@
+package com.javarush.task.task12.task1224;
+
+/* 
+Неведома зверушка
+*/
+
+
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getObjectType(new Cat()));
+        System.out.println(getObjectType(new Tiger()));
+        System.out.println(getObjectType(new Lion()));
+        System.out.println(getObjectType(new Bull()));
+        System.out.println(getObjectType(new Pig()));
+    }
+
+    public static String getObjectType(Object o) {
+        if (o instanceof Cat) {
+            return "Кот";
+        } else if (o instanceof Tiger) {
+            return "Тигр";
+        } else if (o instanceof Lion) {
+            return "Лев";
+        } else if (o instanceof Bull) {
+            return "Бык";
+        } else {
+            return "Животное";
+        }
+    }
+
+    public static class Cat {
+    }
+
+    public static class Tiger {
+    }
+
+    public static class Lion {
+    }
+
+    public static class Bull {
+    }
+
+    public static class Pig {
+    }
+}
