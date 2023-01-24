c69090a7d379a3715dea7d0fbd6b1ab0ef61c0c8
Dmitriy <dmed832@yandex.ru>
2023-01-24 23:30:11 +0300

    add solution JavaCore from task.task12.task1216

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1216/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task12.task1216;
+
+/* 
+Летать охота
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public interface CanFly {
+        void fly ();
+
+        void run();
+    }
+
+}
