be28a59ad6fe4779459b59bfb877437d747049d3
Dmitriy <dmed832@yandex.ru>
2023-01-24 23:32:09 +0300

    add solution JavaCore from task.task12.task1217

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1217/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task12.task1217;
+
+/* 
+Лететь, бежать и плыть
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    interface CanFly {
+        void fly();
+    }
+
+    interface CanRun {
+        void run();
+    }
+
+    interface CanSwim {
+        void swim();
+    }
+
+}
