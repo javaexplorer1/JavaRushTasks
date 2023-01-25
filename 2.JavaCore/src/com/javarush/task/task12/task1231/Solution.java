d6c2d2c165d069de02d3e1176d40e889eba1cbc6
Dmitriy <dmed832@yandex.ru>
2023-01-25 23:45:54 +0300

    add solution JavaCore from task.task12.task1231

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1231/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.task.task12.task1231;
+
+/* 
+Ненужные абстракции
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Horse horse = new Pegasus();
+        horse.run();
+    }
+
+    public static interface CanFly {
+        public abstract void fly();
+    }
+
+    public static  class Horse {
+        public void run() {
+
+        }
+    }
+
+    public static class Pegasus extends Horse implements CanFly {
+        public  void fly() {
+
+        }
+    }
+
+    public static abstract class SwimmingPegasus extends Pegasus {
+        public abstract void swim();
+    }
+
+}
