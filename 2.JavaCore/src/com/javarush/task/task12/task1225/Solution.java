50d4e297cca499e935e8c29b1eb66dc939e23e62
Dmitriy <dmed832@yandex.ru>
2023-01-25 03:50:57 +0300

    add solution JavaCore from task.task12.task1225

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1225/Solution.java
@@ -0,0 +1,52 @@
+package com.javarush.task.task12.task1225;
+
+/* 
+Посетители
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getObjectType(new Cat()));
+        System.out.println(getObjectType(new Tiger()));
+        System.out.println(getObjectType(new Lion()));
+        System.out.println(getObjectType(new Bull()));
+        System.out.println(getObjectType(new Cow()));
+        System.out.println(getObjectType(new Animal()));
+    }
+
+    public static String getObjectType(Object o) {
+        if (o instanceof Tiger) {
+            return "Тигр";
+        } else if (o instanceof Lion) {
+            return "Лев";
+        } else if (o instanceof Cat) {
+            return "Кот";
+        } else if (o instanceof Bull) {
+            return "Бык";
+        } else if (o instanceof Cow) {
+            return "Корова";
+        } else {
+            return "Животное";
+        }
+    }
+
+    public static class Cat extends Animal   //<--Классы наследуются!!
+    {
+    }
+
+    public static class Tiger extends Cat {
+    }
+
+    public static class Lion extends Cat {
+    }
+
+    public static class Bull extends Animal {
+    }
+
+    public static class Cow extends Animal {
+    }
+
+    public static class Animal {
+    }
+
+}
