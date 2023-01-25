53f97ac6b7951ce4dfc488e8736f357641e37ed5
Dmitriy <dmed832@yandex.ru>
2023-01-25 23:36:09 +0300

    add solution JavaCore from task.task12.task1229

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1229/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task12.task1229;
+
+/* 
+Родитель класса CTO
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        CTO cto = new CTO();
+        System.out.println(cto);
+    }
+
+    public static interface Businessman {
+        public void workHard();
+    }
+
+    public static class AdditionalClass implements Businessman {
+        public void workHard() {
+
+        }
+
+    }
+
+    public static class CTO extends AdditionalClass implements Businessman {
+
+    }
+}
