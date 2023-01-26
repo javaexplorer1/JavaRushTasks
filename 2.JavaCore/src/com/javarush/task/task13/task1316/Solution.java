2cac7db8c5b846715541712b938302b91c0b2dd4
Dmitriy <dmed832@yandex.ru>
2023-01-27 01:47:14 +0300

    add solution JavaCore from task.task13.task1316

---
+++ 2.JavaCore/src/com/javarush/task/task13/task1316/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task13.task1316;
+
+/* 
+Некорректные строки
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        System.out.println(SimpleObject.NAME);
+        System.out.println(Button.NAME);
+    }
+
+    interface SimpleObject {
+        String NAME = "SimpleObject";
+    }
+
+    interface Button extends SimpleObject {
+
+        final String NAME = "Submit";
+
+        String onPress(Object o);
+
+
+    }
+}
