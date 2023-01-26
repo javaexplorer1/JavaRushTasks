5dc6d7abf2adb2f7f0c24a8b2e47139401d778f1
Dmitriy <dmed832@yandex.ru>
2023-01-26 23:56:25 +0300

    add solution JavaCore from task.task13.task1311

---
+++ 2.JavaCore/src/com/javarush/task/task13/task1311/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task13.task1311;
+
+/* 
+Нанимаем переводчика
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        EnglishTranslator englishTranslator = new EnglishTranslator();
+        System.out.println(englishTranslator.translate());
+    }
+
+    public static abstract class Translator {
+        public abstract String getLanguage();
+
+        public String translate() {
+            return "Я переводчик с " + getLanguage();
+        }
+    }
+
+
+    static class EnglishTranslator extends Translator {
+        public String getLanguage() {
+            return "английского";
+        }
+
+
+    }
+
+}
