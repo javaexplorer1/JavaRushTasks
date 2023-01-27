194d0c504f8a95af63ba40ac7e3654a4a0318699
Dmitriy <dmed832@yandex.ru>
2023-01-27 23:57:38 +0300

    add solution JavaCore from task.task13.task1322

---
+++ 2.JavaCore/src/com/javarush/task/task13/task1322/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task13.task1322;
+
+/* 
+Интерфейс SimpleObject
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        SimpleObject<String> stringObject = new StringObject();
+    }
+
+    interface SimpleObject<T> {
+        SimpleObject<T> getInstance();
+    }
+
+    static class StringObject implements SimpleObject<String> {
+
+        public SimpleObject<String> getInstance() {
+            return this;
+        }
+
+    }
+
+}
