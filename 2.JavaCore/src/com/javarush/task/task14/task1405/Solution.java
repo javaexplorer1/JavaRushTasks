ffe72899919dc63e11ba291ac59f683fa93dc333
Dmitriy <dmed832@yandex.ru>
2023-01-28 19:36:59 +0300

    add solution JavaCore from task.task14.task1405

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1405/Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.task.task14.task1405;
+
+/* 
+Пора покушать
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Food food = new Food();
+        Selectable selectable = new Food();
+
+        callFoodMethods(food);
+        callSelectableMethods(selectable);
+    }
+
+    public static void callFoodMethods(Food food) {
+        food.onSelect();
+        food.onEat();
+
+    }
+
+    public static void callSelectableMethods(Selectable selectable) {
+        selectable.onSelect();
+    }
+
+    interface Selectable {
+        void onSelect();
+    }
+
+    static class Food implements Selectable {
+        public void onEat() {
+            System.out.println("The food was eaten");
+        }
+
+        public void onSelect() {
+            System.out.println("The food was selected");
+        }
+
+    }
+}
