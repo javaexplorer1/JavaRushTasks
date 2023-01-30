911d065d4f03c3e3d62a6250799126317c4357e5
Dmitriy <dmed832@yandex.ru>
2023-01-30 03:27:33 +0300

    add solution JavaCore from task.task14.task1411

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1411/Solution.java
@@ -0,0 +1,48 @@
+package com.javarush.task.task14.task1411;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+User, Loser, Coder and Proger
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Person person = null;
+
+        while (true) {
+            String key = reader.readLine();
+            if (key.equals("user")) {
+                person = new Person.User();
+            } else if (key.equals("loser")) {
+                person = new Person.Loser();
+            } else if(key.equals("coder")) {
+                person = new Person.Coder();
+            } else if(key.equals("proger")) {
+                person = new Person.Proger();
+            } else break;
+
+            doWork(person);
+
+        }
+    }
+
+    public static void doWork(Person person) {
+        if (person instanceof Person.User) {
+            Person.User user = (Person.User) person;
+            user.live();
+        } else if (person instanceof Person.Loser) {
+            Person.Loser loser = (Person.Loser) person;
+            loser.doNothing();
+        } else if (person instanceof Person.Coder) {
+            Person.Coder coder = (Person.Coder) person;
+            coder.writeCode();
+        } else if (person instanceof Person.Proger) {
+            Person.Proger proger = (Person.Proger) person;
+            proger.enjoy();
+        }
+
+    }
+}
