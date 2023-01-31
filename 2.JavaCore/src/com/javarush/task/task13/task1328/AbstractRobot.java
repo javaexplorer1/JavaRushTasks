aa402555cff06587674d72bb27c3ae68a9cd341d
Dmitriy <dmed832@yandex.ru>
2023-02-01 01:20:45 +0300

    add solution JavaCore from task.task13.task1328

---
+++ 2.JavaCore/src/com/javarush/task/task13/task1328/AbstractRobot.java
@@ -0,0 +1,45 @@
+package com.javarush.task.task13.task1328;
+
+public abstract class AbstractRobot implements Attackable, Defensable {
+
+    private static int hitCount;
+
+    public BodyPart attack() {
+        BodyPart attackedBodyPart = null;
+        hitCount = hitCount + 1;
+
+        if (hitCount == 1) {
+            attackedBodyPart = BodyPart.ARM;
+        } else if (hitCount == 2) {
+            attackedBodyPart = BodyPart.HEAD;
+        } else if (hitCount == 3) {
+            attackedBodyPart = BodyPart.LEG;
+        } else if (hitCount == 4) {
+            hitCount = 0;
+            attackedBodyPart = BodyPart.CHEST;
+        }
+        return attackedBodyPart;
+    }
+
+    public BodyPart defense() {
+        BodyPart defendedBodyPart = null;
+        hitCount = hitCount + 2;
+
+        if (hitCount == 1) {
+            defendedBodyPart = BodyPart.HEAD;
+        } else if (hitCount == 2) {
+            defendedBodyPart = BodyPart.LEG;
+        } else if (hitCount == 3) {
+            defendedBodyPart = BodyPart.CHEST;
+        } else if (hitCount == 4) {
+            hitCount = 0;
+            defendedBodyPart = BodyPart.ARM;
+        }
+        return defendedBodyPart;
+    }
+
+    public abstract String getName();
+
+
+
+}
