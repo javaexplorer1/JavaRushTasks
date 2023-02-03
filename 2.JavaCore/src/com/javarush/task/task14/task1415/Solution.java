6050df9201c5c2ebb23ad576315b2ad8f2328bdb
Dmitriy <dmed832@yandex.ru>
2023-02-04 00:02:37 +0300

    add solution JavaCore from task.task14.task1415

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1415/Solution.java
@@ -0,0 +1,52 @@
+package com.javarush.task.task14.task1415;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Клининговый центр
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        List<Apartment> apartments = new ArrayList<Apartment>();
+        apartments.add(new OneRoomApt());
+        apartments.add(new TwoRoomApt());
+        apartments.add(new ThreeRoomApt());
+
+        cleanAllApartments(apartments);
+    }
+
+    public static void cleanAllApartments(List<Apartment> apartments) {
+        for (Apartment apartment : apartments) {
+            if (apartment instanceof OneRoomApt) {
+                ((OneRoomApt) apartment).clean1Room();
+            } else if (apartment instanceof TwoRoomApt) {
+                ((TwoRoomApt) apartment).clean2Rooms();
+            } else if (apartment instanceof ThreeRoomApt) {
+                ((ThreeRoomApt) apartment).clean3Rooms();
+            }
+        }
+    }
+
+    static interface Apartment {
+    }
+
+    static class OneRoomApt implements Apartment {
+        void clean1Room() {
+            System.out.println("1 room is cleaned");
+        }
+    }
+
+    static class TwoRoomApt implements Apartment {
+        void clean2Rooms() {
+            System.out.println("2 rooms are cleaned");
+        }
+    }
+
+    static class ThreeRoomApt implements Apartment {
+        void clean3Rooms() {
+            System.out.println("3 rooms are cleaned");
+        }
+    }
+}
