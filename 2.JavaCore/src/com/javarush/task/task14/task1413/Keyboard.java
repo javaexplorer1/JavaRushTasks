a927888e801e6aff4331345dcf6df9f3a1e67383
Dmitriy <dmed832@yandex.ru>
2023-02-03 23:57:25 +0300

    add solution JavaCore from task.task14.task1413

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1413/Keyboard.java
@@ -0,0 +1,8 @@
+package com.javarush.task.task14.task1413;
+
+public class Keyboard implements CompItem {
+
+    public String getName() {
+        return "Keyboard";
+    }
+}
---
+++ 2.JavaCore/src/com/javarush/task/task14/task1413/Mouse.java
@@ -0,0 +1,8 @@
+package com.javarush.task.task14.task1413;
+
+public class Mouse implements CompItem {
+
+    public String getName() {
+        return "Mouse";
+    }
+}
