c95272a5789335d537e095702fa4a7ea228a8099
Dmitriy <dmed832@yandex.ru>
2023-02-05 03:22:06 +0300

    add solution JavaSyntax from pro.task18.task1812

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task18/task1812/Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.task.pro.task18.task1812;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.stream.Stream;
+
+/* 
+Рейтинг языков программирования
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        ArrayList<Language> languages = new ArrayList<>();
+        Collections.addAll(languages,
+                new Language("С#", 4.58),
+                new Language("JavaScript", 2.54),
+                new Language("Python", 10.47),
+                new Language("C++", 7.11),
+                new Language("Go", 1.46),
+                new Language("R", 2.37),
+                new Language("Java", 13.48),
+                new Language("C", 15.95),
+                new Language("Swift", 1.38),
+                new Language("PHP", 2.49));
+
+        Stream<Language> languageStream = sortByRanking(languages);
+        languageStream.forEach(System.out::println);
+    }
+
+    public static Stream<Language> sortByRanking(ArrayList<Language> languages) {
+
+//        return languages.stream().sorted((l1, l2) -> Double.compare(l2.getRanking(), l1.getRanking()));
+//        return languages.stream().sorted((l1, l2) -> l2.getRanking().compareTo(l1.getRanking()));
+        return languages.stream().sorted(Comparator.comparingDouble(Language::getRanking).reversed());
+    }
+}
