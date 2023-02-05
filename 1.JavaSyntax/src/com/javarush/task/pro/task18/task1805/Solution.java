d470862dbfb335c94023c8cf4983ec8b051a3e53
Dmitriy <dmed832@yandex.ru>
2023-02-05 03:26:31 +0300

    add solution JavaSyntax from pro.task18.task1805

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task18/task1805/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.pro.task18.task1805;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/* 
+Прощание с лямбда-выражением
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        var strings = new ArrayList<String>();
+
+        Collections.addAll(strings, "JavaRush", "Amigo", "Java Developer", "CodeGym");
+
+        sortStringsByLength(strings);
+
+        for (String string : strings) {
+            System.out.println(string);
+        }
+    }
+
+    public static void sortStringsByLength(ArrayList<String> strings) {
+        Collections.sort(strings, new StringComparator());
+    }
+}
