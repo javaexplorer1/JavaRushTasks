b2d94a665961af67638af5628b2bf90516607bb0
Dmitriy <dmed832@yandex.ru>
2023-02-07 01:09:10 +0300

    add solution JavaCore from task.task14.task1418

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1418/Solution.java
@@ -0,0 +1,52 @@
+package com.javarush.task.task14.task1418;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/* 
+Исправь четыре ошибки
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        List<Number> list = new LinkedList<Number>();
+        //3
+        initList(list);
+        printListValues(list);
+        processCastedObjects(list);
+        //4 - Исправь 2 ошибки
+
+        //5
+
+    }
+
+    public static void initList(List<Number> list) {
+
+        list.add(new Double(1000f));
+        list.add(new Double("123e-445632"));
+        list.add(new Float(-90 / -3));
+        list.remove(new Double("123e-445632"));
+    }
+
+    public static void printListValues(List<Number> list) {
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }
+
+    public static void processCastedObjects(List<Number> list) {
+
+        for (Number object : list) {
+            //Исправь 2 ошибки
+            if (object instanceof Double) {
+                Double a = (Double) object;
+                System.out.println("Is double value infinite? " + a.isInfinite());
+            } else if (object instanceof Float) {
+                Float a = (Float) object;
+                System.out.println("Is float value defined? " + !(a.isNaN()));
+            }
+        }
+    }
+
+
+}
