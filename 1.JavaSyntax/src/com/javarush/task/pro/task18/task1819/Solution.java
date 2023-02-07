4977a37d543ae7be3d41e1b5e0f702011e407b40
Dmitriy <dmed832@yandex.ru>
2023-02-07 22:14:03 +0300

    add solution JavaSyntax from pro.task18.task1819

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task18/task1819/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.pro.task18.task1819;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+/* 
+Как быть, если в списке есть элемент null
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        List<String> strings = new ArrayList<>();
+        Collections.addAll(strings, "first", "second", null, "fourth", "fifth");
+
+        printList(strings);
+    }
+
+    public static void printList(List<String> list) {
+        String text = "Этот элемент равен null";
+//        list.stream().map(x -> Optional.ofNullable(x).orElse(text)).forEach(System.out::println);
+        list.forEach(x -> System.out.println(Optional.ofNullable(x).orElse(text)));
+
+
+
+    }
+}
