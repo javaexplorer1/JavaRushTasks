fe9fa2a67a6bc29a2d5de994787962432b1abfd1
Dmitriy <dmed832@yandex.ru>
2023-02-09 23:55:25 +0300

    add solution JavaCore from task.task16.task1603

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1603/Solution.java
@@ -0,0 +1,46 @@
+package com.javarush.task.task16.task1603;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.IntStream;
+
+/* 
+Список и нити
+*/
+
+public class Solution {
+    public static volatile List<Thread> list = new ArrayList<Thread>(5);
+
+    public static void main(String[] args) throws InterruptedException {
+       SpecialThread specialThread1 = new SpecialThread();
+       SpecialThread specialThread2 = new SpecialThread();
+       SpecialThread specialThread3 = new SpecialThread();
+       SpecialThread specialThread4 = new SpecialThread();
+       SpecialThread specialThread5 = new SpecialThread();
+
+       Thread thread1 = new Thread(specialThread1);
+       Thread thread2 = new Thread(specialThread2);
+       Thread thread3 = new Thread(specialThread3);
+       Thread thread4 = new Thread(specialThread4);
+       Thread thread5 = new Thread(specialThread5);
+
+        list.add(thread1);
+        list.add(thread2);
+        list.add(thread3);
+        list.add(thread4);
+        list.add(thread5);
+
+//        list.forEach(Thread::start);
+
+//        IntStream.range(0, list.size()).forEach(i -> list.add(new Thread(new SpecialThread())));
+//        list.forEach(Thread::start);
+    }
+
+    public static class SpecialThread implements Runnable {
+
+        public void run() {
+            System.out.println("it's a run method inside SpecialThread");
+        }
+    }
+}
