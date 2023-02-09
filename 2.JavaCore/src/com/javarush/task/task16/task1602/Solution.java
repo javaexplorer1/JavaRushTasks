8f7c9d48deeb4990229ad45744cd83d3e0e995e1
Dmitriy <dmed832@yandex.ru>
2023-02-09 23:54:30 +0300

    add solution JavaCore from task.task16.task1602

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1602/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task16.task1602;
+
+/* 
+My second thread
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        TestThread thread = new TestThread();
+        thread.start();
+    }
+
+    public static class TestThread extends Thread {
+
+        static {
+            System.out.println("it's a static block inside TestThread");
+        }
+
+        @Override
+        public void run() {
+            System.out.println("it's a run method");
+        }
+    }
+}
