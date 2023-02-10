eb9203cba8009dd5f1e3d7e1d6cabb90c70154c2
Dmitriy <dmed832@yandex.ru>
2023-02-10 23:53:24 +0300

    add solution JavaCore from task.task15.task1505

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1505/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.task.task15.task1505;
+
+/* 
+ООП - исправь ошибки в наследовании
+*/
+
+public class Solution {
+    public interface HasWeight {
+        int getWeight();
+    }
+
+    public interface HasHeight {
+        int getHeight();
+    }
+
+    public static class Human implements HasWeight, HasHeight {
+        @Override
+        public int getWeight() {
+            return 80;
+        }
+
+        @Override
+        public int getHeight() {
+            return 170;
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        Human human = new Human();
+        System.out.println(human.getWeight());
+        System.out.println(human.getHeight());
+    }
+}
