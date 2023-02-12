c672848fe79586efb4d18a22cfd0f9ceeaac7000
Dmitriy <dmed832@yandex.ru>
2023-02-13 00:10:19 +0300

    add solution JavaCore from task.task15.task1508

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1508/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task15.task1508;
+
+/* 
+ООП - Перегрузка - убираем лишнее
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        print(1);
+    }
+
+
+    public static void print(long l) {
+        System.out.println("Я буду Java прогером!");
+    }
+
+
+
+
+}
