8367a6fcb8c6b2eac637c71bb243676edd47463f
Dmitriy <dmed832@yandex.ru>
2023-02-15 21:47:52 +0300

    add solution JavaCore from task.task15.task1518

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1518/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task15.task1518;
+
+/* 
+Статики и котики
+*/
+
+public class Solution {
+    public static Cat cat;
+
+    static {
+        cat = new Cat();
+        cat.name = "Murzik";
+        System.out.println(cat.name);
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    public static class Cat {
+        public String name;
+
+    }
+
+}
