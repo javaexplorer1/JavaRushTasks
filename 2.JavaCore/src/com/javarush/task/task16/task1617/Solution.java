8bf2a9118a6b616beb364eb243e3f13b0ee2cbf7
Dmitriy <dmed832@yandex.ru>
2023-02-18 22:45:00 +0300

    add solution JavaCore from task.task16.task1617

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1617/Solution.java
@@ -0,0 +1,37 @@
+package com.javarush.task.task16.task1617;
+
+/* 
+Отсчет на гонках
+*/
+
+public class Solution {
+    public static volatile int numSeconds = 4;
+
+    public static void main(String[] args) throws InterruptedException {
+        RacingClock clock = new RacingClock();
+        Thread.sleep(3500);
+        clock.interrupt();
+    }
+
+    public static class RacingClock extends Thread {
+        public RacingClock() {
+            start();
+        }
+
+        public void run() {
+            try {
+                while (numSeconds >= 0) {
+                    if (numSeconds == 0) {
+                        System.out.print("Марш!");
+                    } else {
+                        System.out.print(numSeconds + " ");
+                        Thread.sleep(1000);
+                    }
+                    numSeconds--;
+                }
+            } catch (Exception e) {
+                System.out.println("Прервано!");
+            }
+        }
+    }
+}
