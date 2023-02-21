d2cd7da24f4755f8189a2c1ea51b7ac8d046bba1
Dmitriy <dmed832@yandex.ru>
2023-02-21 20:47:32 +0300

    add solution JavaCore from task.task16.task1610

--- 2.JavaCore/src/com/javarush/task/task16/task1610/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1610/Solution.java
@@ -12,7 +12,7 @@ public class Solution {
 
     private static void investigateWorld() {
         try {
-            Thread.sleep(2000);
+            Thread.sleep(200);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
