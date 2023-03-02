0eb992442723dccacac0557c37abe646b55c5472
Dmitriy <dmed832@yandex.ru>
2023-03-02 21:05:29 +0300

    add solution JavaCore from task.task16.task1626

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1626/Solution.java
@@ -0,0 +1,56 @@
+package com.javarush.task.task16.task1626;
+
+/* 
+Создание по образцу
+*/
+
+public class Solution {
+    public static int number = 5;
+
+    public static void main(String[] args) {
+        new Thread(new CountdownRunnable(), "Уменьшаем").start();
+        new Thread(new CountUpRunnable(), "Увеличиваем").start();
+    }
+
+    public static class CountUpRunnable implements Runnable {
+        private int countIndexUp = 0;
+
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    countIndexUp += 1;
+                    System.out.println(toString());
+                    Thread.sleep(500);
+                    if (countIndexUp == Solution.number) return;
+                }
+            } catch (InterruptedException e) {
+            }
+        }
+
+        public String toString() {
+            return Thread.currentThread().getName() + ": " + countIndexUp;
+        }
+    }
+
+
+    public static class CountdownRunnable implements Runnable {
+        private int countIndexDown = Solution.number;
+
+        public void run() {
+            try {
+                while (true) {
+                    System.out.println(toString());
+                    countIndexDown -= 1;
+                    if (countIndexDown == 0) return;
+                    Thread.sleep(500);
+                }
+            } catch (InterruptedException e) {
+            }
+        }
+
+        public String toString() {
+            return Thread.currentThread().getName() + ": " + countIndexDown;
+        }
+    }
+}
