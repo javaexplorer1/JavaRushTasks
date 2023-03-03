cd07394104038a48ddac501c539ca2a1b2918cbd
Dmitriy <dmed832@yandex.ru>
2023-03-04 00:41:13 +0300

    add solution JavaCore from task.task17.task1701

---
+++ 2.JavaCore/src/com/javarush/task/task17/task1701/Solution.java
@@ -0,0 +1,51 @@
+package com.javarush.task.task17.task1701;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Заметки
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        new NoteThread().start();
+        new NoteThread().start();
+    }
+
+    public static class Note {
+
+        public static final List<String> notes = new ArrayList<>();
+
+        public static void addNote(String note) {
+            notes.add(0, note);
+        }
+
+        public static void removeNote(String threadName) {
+            String note = notes.remove(0);
+            if (note == null) {
+                System.out.println("Другая нить удалила нашу заметку");
+            } else if (!note.startsWith(threadName)) {
+                System.out.println("Нить [" + threadName + "] удалила чужую заметку [" + note + "]");
+            } else {
+                System.out.println("Нить [" + threadName + "] удалила свою заметку [" + note + "]");
+            }
+        }
+    }
+
+
+    public static class NoteThread extends Thread {
+        @Override
+        public void run() {
+            for (int index = 0; index < 1000; index++) {
+                Solution.Note.addNote(getName()+"-Note"+index);
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                Note.removeNote(getName());
+            }
+        }
+    }
+}
