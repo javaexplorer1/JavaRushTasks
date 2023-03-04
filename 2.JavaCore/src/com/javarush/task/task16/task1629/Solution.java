9a376d7f1fb5dadd209427e1a094abd940b496ac
Dmitriy <dmed832@yandex.ru>
2023-03-04 22:16:02 +0300

    add solution JavaCore from task.task16.task1629

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1629/Solution.java
@@ -0,0 +1,46 @@
+package com.javarush.task.task16.task1629;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+Только по-очереди!
+*/
+
+public class Solution {
+    public static volatile BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws InterruptedException {
+        Read3Strings t1 = new Read3Strings();
+        Read3Strings t2 = new Read3Strings();
+        t1.start();
+        t1.join();
+        t2.start();
+        t2.join();
+
+        t1.printResult();
+        t2.printResult();
+    }
+
+    public static class Read3Strings extends Thread {
+        private String string = "";
+
+        @Override
+        public void run() {
+
+            for (int i = 0; i < 3; i++) {
+                try {
+                    string = string + reader.readLine() + " ";
+
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
+        public void printResult() {
+            System.out.println(string);
+        }
+    }
+}
