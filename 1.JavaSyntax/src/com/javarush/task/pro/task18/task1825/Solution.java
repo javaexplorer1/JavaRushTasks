ab9faa73084f5a5936a3b01c2fb364b480344e54
Dmitriy <dmed832@yandex.ru>
2023-03-19 22:45:01 +0300

    add solution JavaSyntax from pro.task18.task1825

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task18/task1825/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.pro.task18.task1825;
+
+import java.util.Collections;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.joining;
+
+/* 
+Из потока данных — в строку
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        var stringStream = Stream.of("To", "be", "a", "programmer", "you", "need", "to", "code");
+
+        System.out.println(getString(stringStream));
+    }
+
+    public static String getString(Stream<String> stringStream) {
+        return stringStream.collect(Collectors.joining(" "));
+
+    }
+}
