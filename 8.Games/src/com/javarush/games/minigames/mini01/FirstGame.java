5a09673efbe15cd319b7883436b67240dbd4a5c9
Dmitriy <dmed832@yandex.ru>
2023-03-22 20:40:26 +0300

    add solution Games from minigames.mini01

---
+++ 8.Games/src/com/javarush/games/minigames/mini01/FirstGame.java
@@ -0,0 +1,10 @@
+package com.javarush.games.minigames.mini01;
+
+import com.javarush.engine.cell.Game;
+
+public class FirstGame extends Game {
+    @Override
+    public void initialize() {
+        super.initialize();
+    }
+}
