58c528f28b8fb276d60c2763ebed45ba042fa0c0
Dmitriy <dmed832@yandex.ru>
2023-03-24 23:38:39 +0300

    add solution JavaCore from task.task19.task1905

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1905/Solution.java
@@ -0,0 +1,96 @@
+package com.javarush.task.task19.task1905;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/* 
+Закрепляем адаптер
+*/
+
+public class Solution {
+    public static Map<String, String> countries = new HashMap<String, String>();
+
+    static {
+        countries.put("UA", "Ukraine");
+        countries.put("RU", "Russia");
+        countries.put("CA", "Canada");
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    public static class DataAdapter implements RowItem{
+
+        private Customer customer;
+        private Contact contact;
+
+        public DataAdapter(Customer customer, Contact contact) {
+            this.customer = customer;
+            this.contact = contact;
+        }
+
+        @Override
+        public String getCountryCode() {
+
+            for (Map.Entry<String, String> pair : countries.entrySet()) {
+                if (pair.getValue().equals(customer.getCountryName())) {
+                    return pair.getKey();
+                }
+            }
+            return "";
+        }
+
+        @Override
+        public String getCompany() {
+            return customer.getCompanyName();
+        }
+
+        @Override
+        public String getContactFirstName() {
+            return contact.getName().split(", ")[1];
+        }
+
+        @Override
+        public String getContactLastName() {
+            return contact.getName().split(", ")[0];
+        }
+
+        @Override
+        public String getDialString() {
+            char[] telChars = contact.getPhoneNumber().toCharArray();
+            StringBuilder sb = new StringBuilder();
+            for (char telChar : telChars) {
+                if (Character.isDigit(telChar)) {
+                    sb.append(telChar);
+                }
+            }
+            return "callto://+" + sb;
+          // or: return "callto://+" + contact.getPhoneNumber().replaceAll("\\D", "");
+        }
+    }
+
+    public static interface RowItem {
+        String getCountryCode();        //For example: UA
+
+        String getCompany();            //For example: JavaRush Ltd.
+
+        String getContactFirstName();   //For example: Ivan
+
+        String getContactLastName();    //For example: Ivanov
+
+        String getDialString();         //For example: callto://+380501234567
+    }
+
+    public static interface Customer {
+        String getCompanyName();        //For example: JavaRush Ltd.
+
+        String getCountryName();        //For example: Ukraine
+    }
+
+    public static interface Contact {
+        String getName();               //For example: Ivanov, Ivan
+
+        String getPhoneNumber();        //For example: +38(050)123-45-67 or +3(805)0123-4567 or +380(50)123-4567 or ...
+    }
+}
