a6af93e88a529b2d86099a53afc52942a126da1c
Dmitriy <dmed832@yandex.ru>
2023-03-26 23:42:45 +0300

    add solution Games from minigames.mini11

---
+++ 8.Games/src/com/javarush/games/minigames/mini11/FillOrangeGame.java
@@ -0,0 +1,18 @@
+package com.javarush.games.minigames.mini11;
+
+import com.javarush.engine.cell.Color;
+
+/* 
+Закрашиваем поле
+*/
+
+public class FillOrangeGame extends FillGame {
+
+    public void fill() {
+        for (int x = 0; x < 5; x++) {
+            for (int y = 0; y < 5; y++) {
+                setCellColor(x, y, Color.ORANGE);
+            }
+        }
+    }
+}
