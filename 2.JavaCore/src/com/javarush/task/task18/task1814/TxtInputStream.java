fee1b62afc75740543007d5e461316bc512e1848
Dmitriy <dmed832@yandex.ru>
2023-03-28 23:53:33 +0300

    add solution JavaCore from task.task18.task1814

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1814/TxtInputStream.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task18.task1814;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+
+/* 
+UnsupportedFileName
+*/
+
+public class TxtInputStream extends FileInputStream {
+
+    public TxtInputStream(String fileName) throws UnsupportedFileNameException, IOException {
+        super(fileName);
+        if (!fileName.endsWith(".txt")) {
+            super.close();
+            throw new UnsupportedFileNameException();
+        }
+    }
+
+
+    public static void main(String[] args) {
+    }
+}
+
