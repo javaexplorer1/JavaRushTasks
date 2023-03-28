1fbf6973f8ea2ee2918948ee360f5c82d437b34c
Dmitriy <dmed832@yandex.ru>
2023-03-28 23:49:16 +0300

    add solution JavaCore from task.task19.task1917

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1917/FileConsoleWriter.java
@@ -0,0 +1,67 @@
+package com.javarush.task.task19.task1917;
+
+import java.io.*;
+
+/* 
+Свой FileWriter
+*/
+
+public class FileConsoleWriter {
+
+    private FileWriter fileWriter;
+
+    public FileConsoleWriter(String fileName) throws IOException {
+        fileWriter = new FileWriter(fileName);
+    }
+
+    public FileConsoleWriter(String fileName, boolean append) throws IOException {
+        fileWriter = new FileWriter(fileName, append);
+    }
+
+    public FileConsoleWriter(File file) throws IOException {
+        fileWriter = new FileWriter(file);
+    }
+
+    public FileConsoleWriter(File file, boolean append) throws IOException {
+        fileWriter = new FileWriter(file, append);
+    }
+
+    public FileConsoleWriter(FileDescriptor fd) {
+        fileWriter = new FileWriter(fd);
+    }
+
+    public void write(char[] cbuf, int off, int len) throws IOException {
+        fileWriter.write(cbuf, off, len);
+        System.out.println(new String(cbuf, off, len));
+    }
+
+    public void write(int c) throws IOException {
+        fileWriter.write(c);
+        System.out.println((char) c);
+    }
+
+    public void write(String str) throws IOException {
+        fileWriter.write(str);
+        System.out.println(str);
+    }
+
+    public void write(String str, int off, int len) throws IOException {
+        fileWriter.write(str, off, len);
+        System.out.println(new String(str.getBytes(), off, len));
+    }
+
+    public void write(char[] cbuf) throws IOException {
+        fileWriter.write(cbuf);
+        System.out.println(new String(cbuf));
+    }
+
+    public void close() throws IOException {
+        fileWriter.close();
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
