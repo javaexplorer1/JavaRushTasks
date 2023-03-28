85d98cb62e5a7a70f7ee36f7b8e1d79ee0794a68
Dmitriy <dmed832@yandex.ru>
2023-03-28 21:23:51 +0300

    add solution JavaCore from task.task18.task1813

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1813/AmigoOutputStream.java
@@ -0,0 +1,51 @@
+package com.javarush.task.task18.task1813;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/* 
+AmigoOutputStream
+*/
+
+public class AmigoOutputStream extends FileOutputStream {
+
+    private FileOutputStream fileOutputStream = null;
+    public static String fileName = "C:/tmp/result.txt";
+
+    public static void main(String[] args) throws FileNotFoundException {
+        new AmigoOutputStream(new FileOutputStream(fileName));
+    }
+
+    public AmigoOutputStream(FileOutputStream fileOutputStream) throws FileNotFoundException {
+        super(fileName);
+        this.fileOutputStream = fileOutputStream;
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        fileOutputStream.write(b);
+    }
+
+    @Override
+    public void close() throws IOException {
+        flush();
+        fileOutputStream.write("JavaRush © All rights reserved.".getBytes());
+        fileOutputStream.close();
+    }
+
+    @Override
+    public void flush() throws IOException {
+        fileOutputStream.flush();
+    }
+
+    @Override
+    public void write(byte[] b) throws IOException {
+        fileOutputStream.write(b);
+    }
+
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+        fileOutputStream.write(b, off, len);
+    }
+}
