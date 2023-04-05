985e5e17f5b565147a430a4195b446c61b561a64
Dmitriy <dmed832@yandex.ru>
2023-04-05 23:54:01 +0300

    add solution JavaCore from task.task19.task1913

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1913/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task19.task1913;
+
+import com.sun.security.jgss.GSSUtil;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+/* 
+Выводим только цифры
+*/
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        PrintStream oldOut = System.out;
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(stream);
+        System.setOut(printStream);
+        testString.printSomething();
+        System.setOut(oldOut);
+        String string = stream.toString();
+        String newString = string.replaceAll("\\D", "");
+        System.out.println(newString);
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's 1 a 23 text 4 f5-6or7 tes8ting");
+        }
+    }
+}
