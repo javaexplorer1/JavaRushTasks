6273f113a2dd1c223f94c4f1c216693995114c3a
Dmitriy <dmed832@yandex.ru>
2023-04-08 22:55:33 +0300

    add solution JavaCore from task.task19.task1920

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1920/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task19.task1920;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Map;
+import java.util.TreeMap;
+
+
+/* 
+Самый богатый
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        Map<String, Double> map = new TreeMap<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(args[0]))) {
+            while (reader.ready()) {
+                String string = reader.readLine();
+                String[] strings = string.split(" ");
+                String name = strings[0];
+                double value = Double.parseDouble(strings[1]);
+                map.merge(name, value, Double::sum);
+            }
+        }
+        double max = map.values().stream().max(Double::compare).get();
+        for (Map.Entry<String, Double> entry : map.entrySet()) {
+            if (entry.getValue() == max) {
+                System.out.println(entry.getKey());
+            }
+        }
+    }
+}
