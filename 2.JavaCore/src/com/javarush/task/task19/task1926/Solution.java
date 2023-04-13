ff014f409377ea21ed8499f0e5f3e2e64a0748a7
Dmitriy <dmed832@yandex.ru>
2023-04-13 23:55:49 +0300

    add solution JavaCore from task.task19.task1926

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1926/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task19.task1926;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+Перевертыши
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader reader = new BufferedReader(new FileReader(console.readLine()))) {
+            while (reader.ready()) {
+                StringBuilder builder = new StringBuilder(reader.readLine());
+                System.out.println(builder.reverse());
+            }
+        }
+    }
+}
