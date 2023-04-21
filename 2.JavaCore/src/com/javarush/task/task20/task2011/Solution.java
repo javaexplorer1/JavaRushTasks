b73f75584ab9b32f8b6983dd89848874238e7853
Dmitriy <dmed832@yandex.ru>
2023-04-21 23:52:35 +0300

    add solution JavaCore from task.task20.task2011

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2011/Solution.java
@@ -0,0 +1,55 @@
+package com.javarush.task.task20.task2011;
+
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+
+/* 
+Externalizable для апартаментов
+*/
+
+public class Solution {
+
+    public static class Apartment implements Externalizable {
+
+        private String address;
+        private int year;
+
+        /**
+         * Mandatory public no-arg constructor.
+         */
+        public Apartment() {
+            super();
+        }
+
+        public Apartment(String addr, int y) {
+            address = addr;
+            year = y;
+        }
+
+        @Override
+        public void writeExternal(ObjectOutput out) throws IOException {
+            out.writeObject(address);
+            out.writeInt(year);
+        }
+
+        @Override
+        public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+            address = (String) in.readObject();
+            year = in.readInt();
+
+        }
+
+        /**
+         * Prints out the fields used for testing!
+         */
+        public String toString() {
+            return ("Address: " + address + "\n" + "Year: " + year);
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
