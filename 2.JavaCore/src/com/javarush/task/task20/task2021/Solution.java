c3fd28b86faa547718dbe4e7209e9affdc0438d3
Dmitriy <dmed832@yandex.ru>
2023-04-24 00:24:12 +0300

    add solution JavaCore from task.task20.task2021

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2021/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task20.task2021;
+
+import java.io.*;
+
+/* 
+Сериализация под запретом
+*/
+
+public class Solution implements Serializable {
+    public static class SubSolution extends Solution {
+
+        private void writeObject(ObjectOutputStream out) throws IOException {
+            throw new NotSerializableException();
+        }
+
+        private void readObject(ObjectInputStream inputStream) throws IOException, ClassNotFoundException {
+            throw new NotSerializableException();
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
