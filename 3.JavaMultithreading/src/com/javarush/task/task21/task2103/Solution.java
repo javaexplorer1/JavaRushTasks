9f727ecb24d2540789d8200092f7d11fd1ba8708
Dmitriy <dmed832@yandex.ru>
2023-04-25 02:23:27 +0300

    add solution JavaCore from task.task21.task2103

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2103/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task21.task2103;
+
+/* 
+Все гениальное - просто!
+*/
+
+public class Solution {
+    public static boolean calculate(boolean a, boolean b, boolean c, boolean d) {
+        return c;
+    }
+
+    public static void main(String[] args) {
+    }
+}
