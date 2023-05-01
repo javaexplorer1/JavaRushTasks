49004125ddce77ba226a51c38cd9e279d59e0dd4
Dmitriy <dmed832@yandex.ru>
2023-05-01 23:52:57 +0300

    add solution JavaCore from task.task21.task2108

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2108/Solution.java
@@ -0,0 +1,77 @@
+package com.javarush.task.task21.task2108;
+
+/* 
+Клонирование растений
+*/
+
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+        Tree tree = new Tree("willow", new String[]{"s1", "s2", "s3", "s4"});
+        Tree clone = null;
+        try {
+            clone = tree.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(tree);
+        System.out.println(clone);
+
+        System.out.println(tree.branches);
+        System.out.println(clone.branches);
+    }
+
+    public static class Plant {
+        private String name;
+
+        public Plant(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public Plant() {
+        }
+    }
+
+    public static class Tree extends Plant implements Cloneable {
+        private String[] branches;
+
+        public Tree() {
+        }
+
+        public Tree(String name, String[] branches) {
+            super(name);
+            this.branches = branches;
+        }
+
+        public String[] getBranches() {
+            return branches;
+        }
+
+        @Override
+        protected Tree clone() throws CloneNotSupportedException {
+            Tree tree = new Tree();
+            String[] newBranches = Arrays.copyOf(branches, branches.length);
+            tree.branches = newBranches;
+            return tree;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Tree tree = (Tree) o;
+            return Arrays.equals(branches, tree.branches);
+        }
+
+        @Override
+        public int hashCode() {
+            return Arrays.hashCode(branches);
+        }
+    }
+}
