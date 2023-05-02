251707a18fc606fb0ea8dad750b45736addcd4bd
Dmitriy <dmed832@yandex.ru>
2023-05-02 22:56:57 +0300

    add solution JavaCore from task.task21.task2109

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2109/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2109/Solution.java
@@ -78,9 +78,7 @@ public class Solution {
 
     public static class C extends B implements Cloneable {
 
-        int i;
-        int j;
-        String name;
+
 
         public C(int i, int j, String name) {
             super(i, j, name);
@@ -88,13 +86,36 @@ public class Solution {
 
         @Override
         protected C clone() throws CloneNotSupportedException {
-            C c = new C(i, j, name);
-
+            C c = new C(getI(), getJ(), getName());
             return c;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            return super.equals(o);
+        }
+
+        @Override
+        public int hashCode() {
+            return super.hashCode();
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws CloneNotSupportedException {
+
+        A objectA = new A(2, 7);
+        Object cloneA = objectA.clone();
+        System.out.println(objectA.equals(cloneA));
+
+/*
+        B objectB = new B(2, 7, "Tom");
+        Object cloneB = objectB.clone();
+        System.out.println(objectB.equals(cloneB));
+*/
+
+        C objectC = new C(2, 5, "Tom");
+        Object cloneC = objectC.clone();
+        System.out.println(objectC.equals(cloneC));
 
     }
 
