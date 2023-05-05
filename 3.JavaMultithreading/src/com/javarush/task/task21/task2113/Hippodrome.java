04ed9756c13e82b79fdc295661d3b278617b9883
Dmitriy <dmed832@yandex.ru>
2023-05-05 23:31:52 +0300

    add solution JavaCore from task.task21.task2113 (add horses and getHorses)

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -1,7 +1,15 @@
 package com.javarush.task.task21.task2113;
 
-public class Hippodrome {
-    public static void main(String[] args) {
+import java.util.List;
 
+public class Hippodrome {
+
+    private List<Horse> horses;
+
+    public List<Horse> getHorses() {
+        return horses;
+    }
+    public static void main(String[] args) {
+        
     }
 }
