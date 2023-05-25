fee024fb8632cee927745c424a1aaf3fcdf7a121
Dmitriy <dmed832@yandex.ru>
2023-05-25 19:39:20 +0300

    add solution SQL & Hibernate from task.task02.task0219 (check on real table)

--- 3.JavaMultithreading/src/com/javarush/task/sql/task02/task0219/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task02/task0219/query.sql
@@ -1,2 +1,3 @@
 -- Write your code here:
-SELECT IF(IFNULL(euro, 'good') = 'good', 'good', 'bad');
+SELECT IF(IFNULL(euro, 'good') = 'good', 'good', 'bad') FROM cars;
+-- SELECT department, IF(IFNULL(department, 'good') = 'good', 'good', 'bad') FROM employee;
