bd9cb169730fc100a6c0ca236121eeee6d501615
Dmitriy <dmed832@yandex.ru>
2023-05-31 00:01:42 +0300

    add solution SQL & Hibernate from task.task02.task0235 (corrected request)

--- 3.JavaMultithreading/src/com/javarush/task/sql/task02/task0235/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task02/task0235/query.sql
@@ -1,2 +1,2 @@
 -- Write your code here:
-SELECT YEAR(date_of_birth) AS year_of_birth, COUNT(*) FROM employee WHERE position LIKE '%developer%';
+SELECT YEAR(date_of_birth) AS year_of_birth, COUNT(*) FROM employee WHERE position LIKE '%developer%' GROUP BY year_of_birth;
