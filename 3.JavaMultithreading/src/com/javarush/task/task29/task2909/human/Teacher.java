e9b5c9f042b2d672fdfb6d17189d4707b954f7bb
Dmitriy <dmed832@yandex.ru>
2023-05-31 23:25:04 +0300

    add solution JavaMultithreading from task.task29.task2909

---
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Teacher.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task29.task2909.human;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Teacher extends Human {
+
+    private int numberOfStudents;
+    private String university;
+
+    public Teacher(String name, int age, int numberOfStudents) {
+        super(false);
+        this.name = name;
+        this.age = age;
+        this.numberOfStudents = numberOfStudents;
+    }
+
+    public void live() {
+        teach();
+    }
+
+    public void teach() {
+    }
+
+    public String getUniversity() {
+        return university;
+    }
+
+    public void setUniversity(String university) {
+        this.university = university;
+    }
+
+    public void printData() {
+        System.out.println("Преподаватель: " + name);
+    }
+}
