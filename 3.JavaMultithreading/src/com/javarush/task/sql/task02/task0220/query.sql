53cad37d786aca88b910dbe43b3f4cca79193b1a
Dmitriy <dmed832@yandex.ru>
2023-06-03 23:44:44 +0300

    add solution SQL & Hibernate from task.task02.task0220 (query is simplified)

--- 3.JavaMultithreading/src/com/javarush/task/sql/task02/task0220/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task02/task0220/query.sql
@@ -1,8 +1,8 @@
 -- Write your code here:
 SELECT DISTINCT name,
-                CASE
-                    WHEN prod_year = 2020 THEN 'new'
-                    WHEN prod_year = 2021 THEN 'newer'
-                    WHEN prod_year = 2022 THEN 'even newer'
+                CASE prod_year
+                    WHEN 2020 THEN 'new'
+                    WHEN 2021 THEN 'newer'
+                    WHEN 2022 THEN 'even newer'
                 END
 FROM cars;
