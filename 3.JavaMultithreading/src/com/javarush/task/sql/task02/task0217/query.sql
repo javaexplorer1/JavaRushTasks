4da79e5278de22c8539ff4f8936de677de76d72f
Dmitriy <dmed832@yandex.ru>
2023-06-03 23:38:14 +0300

    add solution SQL & Hibernate from task.task02.task0217 (query is formatted)

--- 3.JavaMultithreading/src/com/javarush/task/sql/task02/task0217/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task02/task0217/query.sql
@@ -1,8 +1,11 @@
 -- Write your code here:
 SELECT
-CASE
-WHEN euro > 5 THEN 'best'
-WHEN euro = 5 THEN 'good'
-ELSE 'bad'
-END
-FROM cars WHERE prod_year > 2020 ORDER BY price LIMIT 5;
+    CASE
+        WHEN euro > 5 THEN 'best'
+        WHEN euro = 5 THEN 'good'
+        ELSE 'bad'
+    END
+FROM cars
+WHERE prod_year > 2020
+ORDER BY price
+LIMIT 5;
