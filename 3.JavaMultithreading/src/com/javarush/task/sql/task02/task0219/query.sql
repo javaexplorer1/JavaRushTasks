4fe7333315ff30af4829f798b899c5ccf33214ed
Dmitriy <dmed832@yandex.ru>
2023-06-03 23:42:21 +0300

    add solution SQL & Hibernate from task.task02.task0219 (query is simplified)

--- 3.JavaMultithreading/src/com/javarush/task/sql/task02/task0219/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task02/task0219/query.sql
@@ -1,3 +1,9 @@
 -- Write your code here:
-SELECT IF(IFNULL(euro, 'good') = 'good', 'good', 'bad') FROM cars;
+-- SELECT IF(IFNULL(euro, 'good') = 'good', 'good', 'bad') FROM cars;
 -- SELECT department, IF(IFNULL(department, 'good') = 'good', 'good', 'bad') FROM employee;
+SELECT
+    CASE
+        WHEN euro IS NULL THEN 'good'
+        ELSE 'bad'
+    END
+FROM cars;
