06e362609fac2ab9aff27fb36757ee08251a9d0c
Dmitriy <dmed832@yandex.ru>
2023-06-05 23:55:26 +0300

    add solution SQL & Hibernate from task.task02.task0231 (fixed and formatted query)

--- 3.JavaMultithreading/src/com/javarush/task/sql/task02/task0231/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task02/task0231/query.sql
@@ -1,2 +1,4 @@
 -- Write your code here:
-SELECT department AS department_name, COUNT(*) AS count FROM employee WHERE position = 'frontend developer' OR position = 'backend developer';
+SELECT department AS department_name, COUNT(*) AS count
+FROM employee
+WHERE position = 'frontend developer' OR position = 'backend developer' GROUP BY department;
