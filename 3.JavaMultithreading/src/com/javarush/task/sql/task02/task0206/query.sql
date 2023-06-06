7b74f4c284013474480f2e45c286ff9af0577a56
Dmitriy <dmed832@yandex.ru>
2023-06-06 23:33:09 +0300

    add solution SQL & Hibernate from task.task02.task0206 (fixed query)

--- 3.JavaMultithreading/src/com/javarush/task/sql/task02/task0206/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task02/task0206/query.sql
@@ -1,3 +1,3 @@
 -- Write your code here:
-SELECT id, name, age, smth, salary, department, IF(salary <= 500, 1000, 0) AS bonus FROM employee
+SELECT id, name, age, smth, salary, department, IF(salary <= 500, 1000, 0) FROM employee
 WHERE id > 5 LIMIT 10;
