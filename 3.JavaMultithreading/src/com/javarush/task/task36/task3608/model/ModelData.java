a275da96a30ad5fea42b4bb3c81036c71a358c24
Dmitriy <dmed832@yandex.ru>
2023-06-25 02:25:09 +0400

    add solution JavaMultithreading from task.task36.task3608

---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/ModelData.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task36.task3608.model;
+
+import com.javarush.task.task36.task3608.bean.User;
+
+import java.util.List;
+
+public class ModelData {
+    private List<User> users;
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+}
