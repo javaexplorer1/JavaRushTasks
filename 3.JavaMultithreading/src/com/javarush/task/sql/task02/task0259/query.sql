050faf173f0fdea9545979eb465e127695c69ea2
Dmitriy <dmed832@yandex.ru>
2023-06-25 23:36:49 +0400

    add solution SQL & Hibernate from task.task02.task0259 (fixed query)

--- 3.JavaMultithreading/src/com/javarush/task/sql/task02/task0259/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task02/task0259/query.sql
@@ -1,2 +1,5 @@
 -- Write your code here:
-SELECT position, GROUP_CONCAT(name SEPARATOR ';') FROM employee GROUP BY position HAVING position = '%developer%';
+SELECT position, GROUP_CONCAT(name SEPARATOR ';')
+FROM employee
+GROUP BY position
+HAVING position LIKE '%developer%';
