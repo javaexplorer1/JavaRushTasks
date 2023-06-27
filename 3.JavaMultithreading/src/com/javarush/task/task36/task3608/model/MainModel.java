bb1f1a86ed9e2f5ea79c500c9e2ea9939db48cb7
Dmitriy <dmed832@yandex.ru>
2023-06-27 23:52:51 +0400

    add solution JavaMultithreading from task.task36.task3608 (4)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/MainModel.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task36.task3608.model;
+
+import com.javarush.task.task36.task3608.bean.User;
+import com.javarush.task.task36.task3608.model.service.UserService;
+import com.javarush.task.task36.task3608.model.service.UserServiceImpl;
+
+import java.util.List;
+
+public class MainModel implements Model{
+
+    private UserService userService = new UserServiceImpl();
+    private ModelData modelData = new ModelData();
+
+    @Override
+    public ModelData getModelData() {
+        return modelData;
+    }
+
+    @Override
+    public void loadUsers() {
+        List<User> users = userService.getUsersBetweenLevels(1, 100);
+        modelData.setUsers(users);
+    }
+}
