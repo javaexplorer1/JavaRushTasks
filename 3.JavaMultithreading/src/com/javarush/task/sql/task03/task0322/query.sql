b620e8c0be46055ae7d7d26ecd6a08bbdddd0716
Dmitriy <dmed832@yandex.ru>
2023-06-29 00:00:09 +0400

    add solution SQL & Hibernate from task.task03.task0322 (fixed query)

--- 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0322/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0322/query.sql
@@ -5,4 +5,4 @@ SELECT c.customer_id,
        o.order_status
 FROM customers AS c JOIN orders AS o
 ON c.customer_id = o.customer_id
-WHERE o.order_status LIKE '%PAID%' OR o.order_status LIKE 'PROCESSING';
+WHERE o.order_status = 'PAID' OR o.order_status = 'PROCESSING';
