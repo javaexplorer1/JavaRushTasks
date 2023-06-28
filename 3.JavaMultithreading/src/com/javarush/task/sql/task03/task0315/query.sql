58918465203c2cdc475ee48bea3f08d26c086398
Dmitriy <dmed832@yandex.ru>
2023-06-28 23:59:07 +0400

    add solution SQL & Hibernate from task.task03.task0315 (fixed query)

--- 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0315/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0315/query.sql
@@ -2,4 +2,4 @@
 SELECT ret.hq_location AS ret_location,
        sup.annual_revenue_billions AS sup_revenue
 FROM top_retailers AS ret, suppliers AS sup
-WHERE ret_location <> 'USA';
+WHERE ret_location != 'USA';
