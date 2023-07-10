5722851c39cfc9df63064a95291d08ed61da8d43
Dmitriy <dmed832@yandex.ru>
2023-07-10 18:18:57 +0400

    add solution SQL & Hibernate from task.task04.task0423 (fixed query)

--- 3.JavaMultithreading/src/com/javarush/task/sql/task04/task0423/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task04/task0423/query.sql
@@ -1,2 +1,2 @@
 -- Write your code here:
-SELECT name, position FROM employee WHERE DAYNAME(day_birth) = 'понедельник';
+SELECT name, position FROM employee WHERE DAYNAME(date_birth) = 'понедельник';
