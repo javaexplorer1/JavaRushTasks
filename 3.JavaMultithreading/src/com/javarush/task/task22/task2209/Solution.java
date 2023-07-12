537111c248d43761f4b2167f965db6168be1fa06
Dmitriy <dmed832@yandex.ru>
2023-07-12 23:17:51 +0400

    add solution JavaMultithreading from task.task22.task2209

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2209/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2209/Solution.java
@@ -1,9 +1,10 @@
 package com.javarush.task.task22.task2209;
 
-import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 /*
 Составить цепочку слов
@@ -21,33 +22,48 @@ public class Solution {
         System.out.println(getLine(strings));
     }
 
-
     public static StringBuilder getLine(String... words) {
-        StringBuilder builder = new StringBuilder("");
-        if (words == null) {
+        StringBuilder builder = new StringBuilder();
+        if (words == null || words.length == 0) {
             return builder;
         }
-        List<String> list = new ArrayList<>(Arrays.asList(words));
-        builder.append(list.get(0));
-        list.remove(0);
-        do {
-            for (int i = 0; i < list.size(); i++) {
+        List<StringBuilder> builderList = new ArrayList<>();
+        for (String word : words) {
+
+            builder.append(word);
+
+            List<String> list = new ArrayList<>(Arrays.asList(words));
+            list.remove(word);
+
+            int count = 0;
+            while (!list.isEmpty()) {
+
                 String firstStartChar = builder.substring(0, 1);
                 String firstEndChar = builder.substring(builder.length() - 1);
-                String secondStartChar = list.get(i).substring(0, 1);
-                String secondEndChar = list.get(i).substring(list.get(i).length() - 1);
+
+                String secondStartChar = list.get(0).substring(0, 1);
+                String secondEndChar = list.get(0).substring(list.get(0).length() - 1);
+
                 if (firstEndChar.equalsIgnoreCase(secondStartChar)) {
-                    builder.append(" ").append(list.get(i));
-                    list.remove(i);
-                    i--;
+                    builder.append(" ").append(list.get(0));
+                    list.remove(0);
                 } else if (firstStartChar.equalsIgnoreCase(secondEndChar)) {
-                    builder.insert(0, list.get(i) + " ");
-                    list.remove(i);
-                    i--;
+                    builder.insert(0, list.get(0) + " ");
+                    list.remove(0);
+                } else {
+                    String str = list.get(0);
+                    list.remove(0);
+                    list.add(str);
+                    count++;
+                    if (count > words.length * word.length()) {
+                        break;
+                    }
                 }
             }
-        } while (!list.isEmpty());
-        return builder;
+            builderList.add(builder);
+            builder = new StringBuilder();
+        }
+        return builderList.stream().max(Comparator.comparingInt(StringBuilder::length)).get();
     }
 
 
