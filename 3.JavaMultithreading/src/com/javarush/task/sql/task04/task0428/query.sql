0e201081d2b7ce5a35552b169708279834de2d69
Dmitriy <dmed832@yandex.ru>
2023-07-12 22:15:49 +0400

    add solution SQL & Hibernate from task.task04.task0428 (fixed query)

--- 3.JavaMultithreading/src/com/javarush/task/sql/task04/task0428/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task04/task0428/query.sql
@@ -1,2 +1,2 @@
 -- Write your code here:
-SELECT * FROM event WHERE MONTH(date_time) = MONTH(CURDATE());
+SELECT * FROM event WHERE MONTH(date_time) = MONTH(CURDATE()) AND YEAR(date_time) = YEAR(CURDATE());
