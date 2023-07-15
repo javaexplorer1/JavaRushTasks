f7df30a08744ebbb8fa62cfb805f0bc93abd81eb
Dmitriy <dmed832@yandex.ru>
2023-07-15 23:23:52 +0400

    add solution JavaMultithreading from task.task30.task3008 (10)

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -74,5 +74,18 @@ public class Server {
                 }
             }
         }
+
+        private void serverMainLoop(Connection connection, String userName) throws IOException, ClassNotFoundException {
+            while (true) {
+                Message message = connection.receive();
+
+                if (message.getType() == MessageType.TEXT) {
+                    sendBroadcastMessage(new Message(MessageType.TEXT,
+                            userName + ": " + message.getData()));
+                } else {
+                    ConsoleHelper.writeMessage("Сообщение не является текстом");
+                }
+            }
+        }
     }
 }
