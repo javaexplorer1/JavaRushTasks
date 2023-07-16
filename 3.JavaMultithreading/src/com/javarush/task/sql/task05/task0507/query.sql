e1df7449ee6e8b0b271c9cf97e3b16500ec52b46
Dmitriy <dmed832@yandex.ru>
2023-07-17 01:16:33 +0400

    add solution SQL & Hibernate from task.task05.task0507

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0507/query.sql
@@ -0,0 +1,5 @@
+-- Write your code here:
+UPDATE part SET
+name = CASE WHEN id = 92909 THEN 'Technic, Steering Wheel Hub 3 Pin Round' ELSE name END,
+name = CASE WHEN id = 19916 THEN 'Minifigure, Headgear Helmet SW Darth Vader Type 2 Top' ELSE name END
+WHERE id IN (92909, 19916);
