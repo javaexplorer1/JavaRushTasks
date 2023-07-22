8cbff15281dc3e1add878d14317052ded4225f48
Dmitriy <dmed832@yandex.ru>
2023-07-23 02:54:07 +0400

    add solution SQL & Hibernate from task.task05.task0526 (JOIN and ON on the same line)

--- 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0526/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0526/query.sql
@@ -1,6 +1,4 @@
 -- Write your code here:
 SELECT book.publication_year, book.isbn, book.title, author.full_name AS author, publisher.name AS publisher
-FROM author JOIN book
-ON author.id = book.author_id
-JOIN publisher
-ON book.publisher_id = publisher.id;
+FROM book JOIN author ON book.author_id = author.id
+            JOIN publisher ON publisher.id = book.publisher_id;
