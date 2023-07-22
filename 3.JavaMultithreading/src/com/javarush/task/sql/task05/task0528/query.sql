1af5486d09ddd8e3848d09b50c55bc289e6717d5
Dmitriy <dmed832@yandex.ru>
2023-07-22 21:57:52 +0400

    add solution SQL & Hibernate from task.task05.task0528

--- 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0528/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0528/query.sql
@@ -1,1 +1,7 @@
 -- Write your code here:
+SELECT publisher.name AS name
+FROM publisher
+WHERE publisher.id IN
+      (SELECT publisher.id FROM publisher JOIN book ON publisher.id = book.publisher_id
+                                          JOIN author ON publisher.id = author.id
+                                            WHERE book.title LIKE 'Mark Twain');
