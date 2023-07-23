a4b502c1b42094a7b577fbcdfb7db92f80efc0b6
Dmitriy <dmed832@yandex.ru>
2023-07-23 04:17:54 +0400

    add solution SQL & Hibernate from task.task05.task0527 (fixed query)

--- 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0527/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0527/query.sql
@@ -1,6 +1,4 @@
 -- Write your code here:
-USE library;
 SELECT DISTINCT book.title
-FROM book JOIN author
-ON book.author_id = author.id
-WHERE author.full_name LIKE 'Edgar Allan Poe';
+FROM book JOIN author ON book.author_id = author.id
+WHERE author.full_name = 'Edgar Allan Poe';
