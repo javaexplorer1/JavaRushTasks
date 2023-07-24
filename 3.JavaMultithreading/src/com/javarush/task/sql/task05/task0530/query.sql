09d28b1f2d05124744103456afa336450c0046c6
Dmitriy <dmed832@yandex.ru>
2023-07-24 23:59:35 +0400

    add solution SQL & Hibernate from task.task05.task0530

--- 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0530/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0530/query.sql
@@ -1,1 +1,7 @@
 -- Write your code here:
+USE space;
+SELECT name,
+       YEAR(discovery_date) AS d_year,
+       MONTHNAME(discovery_date) AS d_month,
+       DAYOFWEEK(discovery_date) AS d_day
+FROM object;
