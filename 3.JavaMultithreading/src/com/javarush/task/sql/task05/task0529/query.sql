7a7070faf584d5a226f0d1754e1de10468fb6cce
Dmitriy <dmed832@yandex.ru>
2023-07-24 23:55:49 +0400

    add solution SQL & Hibernate from task.task05.task0529

--- 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0529/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0529/query.sql
@@ -1,1 +1,4 @@
 -- Write your code here:
+SELECT author.full_name AS full_name, IF(COUNT(*) = 0, 0, COUNT(*)) AS publishers
+FROM author LEFT JOIN book ON author.id = book.author_id
+                JOIN publisher ON book.publisher_id = publisher.id;
