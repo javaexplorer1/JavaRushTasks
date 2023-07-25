ce5be3274fd79ab35707dfed193a2fe099a07c41
Dmitriy <dmed832@yandex.ru>
2023-07-25 19:50:00 +0400

    add solution SQL & Hibernate from task.task05.task0530 (fixed query)

--- 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0530/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0530/query.sql
@@ -3,5 +3,5 @@ USE space;
 SELECT name,
        YEAR(discovery_date) AS d_year,
        MONTHNAME(discovery_date) AS d_month,
-       DAYOFWEEK(discovery_date) AS d_day
+       DAYNAME(discovery_date) AS d_day
 FROM object;
