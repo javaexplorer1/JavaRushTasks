62338f6ac1914fb9b56e7e661b38c6e582776b3d
Dmitriy <dmed832@yandex.ru>
2023-07-27 21:38:20 +0400

    add solution SQL & Hibernate from task.task06.task0614

--- 3.JavaMultithreading/src/com/javarush/task/sql/task06/task0614/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task06/task0614/query.sql
@@ -1,1 +1,6 @@
 -- Write your code here:
+UPDATE employee, (SELECT employee.id FROM employee JOIN task ON employee.id = task.employee_id
+    WHERE task.exp_date < '2022-10-01') AS id_to_update SET
+    salary = salary + 1000
+WHERE employee.id = id_to_update.id;
+
