5e11ff8dd84bce16a98b09f10278c8433a634433
Dmitriy <dmed832@yandex.ru>
2023-07-28 03:47:32 +0400

    add solution SQL & Hibernate from task.task06.task0604 (query formatting)

--- 3.JavaMultithreading/src/com/javarush/task/sql/task06/task0604/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task06/task0604/query.sql
@@ -1,6 +1,7 @@
 -- Write your code here:
-CREATE TABLE users (
-    user_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE users
+(
+    user_id    INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL
+    last_name  VARCHAR(255) NOT NULL
 );
