74b24e1275e934dbbf6a4faa412a75d9af111dc9
Dmitriy <dmed832@yandex.ru>
2023-07-28 23:45:03 +0400

    add solution SQL & Hibernate from task.task06.task0609 (fixed query)

--- 3.JavaMultithreading/src/com/javarush/task/sql/task06/task0609/query.sql
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task06/task0609/query.sql
@@ -1,4 +1,4 @@
 -- Write your code here:
 INSERT INTO sale_addresses (street, city, state, zip_code, country)
 SELECT street, city, state, zip_code, country
-FROM customers WHERE country != 'Testostan';
+FROM customers WHERE country NOT LIKE '%Testostan%';
