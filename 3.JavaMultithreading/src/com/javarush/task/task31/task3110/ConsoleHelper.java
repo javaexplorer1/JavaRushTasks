9e71473fa8631567f4ba3d94099f2e9320cb68e1
Dmitriy <dmed832@yandex.ru>
2023-08-10 23:54:50 +0400

    add solution JavaMultithreading from task.task31.task3110 (3)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ConsoleHelper.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task31.task3110;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ConsoleHelper {
+
+    private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() throws IOException {
+        return reader.readLine();
+    }
+
+    public static int readInt() throws IOException {
+        return Integer.parseInt(reader.readLine());
+    }
+
+
+}
