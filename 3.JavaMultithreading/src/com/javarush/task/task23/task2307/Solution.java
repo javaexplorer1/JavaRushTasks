2d5d9cdd143c286e9c489e40021060dc6535f5f5
Dmitriy <dmed832@yandex.ru>
2023-08-27 23:57:09 +0300

    add solution JavaMultithreading from task.task23.task2307

---
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2307/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task23.task2307;
+
+/* 
+Как выбрать нужное?
+*/
+
+public class Solution {
+    public static final String TEST = "test";
+
+    public static class TEST {
+        @Override
+        public String toString() {
+            return "test class";
+        }
+    }
+
+    static Object obj;
+
+    public static void main(String[] args) {
+        obj = Solution.TEST;
+        System.out.println(obj);
+        obj = new TEST();
+        System.out.println(obj);
+    }
+}
