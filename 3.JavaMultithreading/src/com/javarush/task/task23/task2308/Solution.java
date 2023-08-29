e8f76ec35fcd28fa57a2a3440a669c2190df7323
Dmitriy <dmed832@yandex.ru>
2023-08-29 23:54:22 +0300

    add solution JavaMultithreading from task.task23.task2308

---
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2308/Solution.java
@@ -0,0 +1,61 @@
+package com.javarush.task.task23.task2308;
+
+/* 
+Рефакторинг, вложенные классы
+*/
+
+import static com.javarush.task.task23.task2308.Solution.Constants.*;
+
+public class Solution {
+    public class ServerNotAccessibleException extends Exception {
+        public ServerNotAccessibleException() {
+            super(SERVER_IS_CURRENTLY_NOT_ACCESSIBLE);
+        }
+
+        public ServerNotAccessibleException(Throwable cause) {
+            super(SERVER_IS_CURRENTLY_NOT_ACCESSIBLE, cause);
+        }
+    }
+
+    public class UnauthorizedUserException extends Exception {
+        public UnauthorizedUserException() {
+            super(USER_IS_NOT_AUTHORIZED);
+        }
+
+        public UnauthorizedUserException(Throwable cause) {
+            super(USER_IS_NOT_AUTHORIZED, cause);
+        }
+    }
+
+    public class BannedUserException extends Exception {
+        public BannedUserException() {
+            super(USER_IS_BANNED);
+        }
+
+        public BannedUserException(Throwable cause) {
+            super(USER_IS_BANNED, cause);
+        }
+    }
+
+    public class RestrictionException extends Exception {
+        public RestrictionException() {
+            super(ACCESS_IS_DENIED);
+        }
+
+        public RestrictionException(Throwable cause) {
+            super(ACCESS_IS_DENIED, cause);
+        }
+    }
+
+    public final class Constants {
+        public static final String SERVER_IS_CURRENTLY_NOT_ACCESSIBLE = "The server is not currently accessible.";
+        public static final String USER_IS_NOT_AUTHORIZED = "The user is not authorized.";
+        public static final String USER_IS_BANNED = "The user is banned.";
+        public static final String ACCESS_IS_DENIED = "Access is denied.";
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
