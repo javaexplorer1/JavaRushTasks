1aad4f1d9174bc1b6f040e65b286a696e185854f
Dmitriy <dmed832@yandex.ru>
2023-09-02 22:59:00 +0300

    add solution JavaMultithreading from task.task32.task3209 (3)

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -7,6 +7,10 @@ import java.awt.event.ActionListener;
 public class View extends JFrame implements ActionListener {
 
     private Controller controller;
+    private JTabbedPane tabbedPane = new JTabbedPane();
+    private JTextPane htmlTextPane = new JTextPane();
+    private JEditorPane plainTextPane = new JEditorPane();
+
 
     public Controller getController() {
         return controller;
