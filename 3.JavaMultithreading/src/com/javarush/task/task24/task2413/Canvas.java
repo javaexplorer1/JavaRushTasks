710c8df343246ce64af407e4f9380e2a6744dd0f
Dmitriy <dmed832@yandex.ru>
2023-09-08 23:57:25 +0300

    add solution JavaMultithreading from task.task24.task2413 (10)

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
@@ -1,5 +1,7 @@
 package com.javarush.task.task24.task2413;
 
+import static java.lang.Math.round;
+
 public class Canvas {
     private int width;
     private int height;
@@ -33,5 +35,22 @@ public class Canvas {
 
     public void setMatrix(char[][] matrix) {
         this.matrix = matrix;
+    }
+
+    public void setPoint(double x, double y, char c) {
+        if (x >= 0 && y >= 0 && y < matrix.length && x < matrix[0].length) {
+            matrix[(int) Math.round(y)][(int) Math.round(x)] = c;
+        }
+    }
+
+    public void drawMatrix(double x, double y, int[][] matrix, char c) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] != 0) {
+                    setPoint(x + j, y + i, c);
+                }
+            }
+
+        }
     }
 }
