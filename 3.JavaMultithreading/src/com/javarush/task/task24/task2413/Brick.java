d4a049064f2a5a7cd7c5e2fa2159bfd080091ba0
Dmitriy <dmed832@yandex.ru>
2023-09-10 23:48:57 +0300

    add solution JavaMultithreading from task.task24.task2413 (12)

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Brick.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Brick.java
@@ -1,8 +1,8 @@
 package com.javarush.task.task24.task2413;
 
 public class Brick extends BaseObject {
-    public Brick(double x, double y, double radius) {
-        super(x, y, radius);
+    public Brick(double x, double y) {
+        super(x, y, 3);
     }
 
     @Override
