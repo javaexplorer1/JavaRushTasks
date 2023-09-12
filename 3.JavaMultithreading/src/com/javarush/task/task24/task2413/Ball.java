5fe22c73f9168085aeb97866bd9abd9f573cf87e
Dmitriy <dmed832@yandex.ru>
2023-09-12 23:30:28 +0300

    add solution JavaMultithreading from task.task24.task2413 (15)

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
@@ -49,5 +49,17 @@ public class Ball extends BaseObject {
         isFrozen = false;
     }
 
+    public void setDirection(double direction) {
+        this.direction = direction;
+        double angle = Math.toRadians(direction);
+        dx = Math.cos(angle) * speed;
+        dy = Math.sin(angle) * speed;
+    }
+
+    public void checkRebound(int minx, int maxx, int miny, int maxy) {
+
+    }
+
+
 
 }
