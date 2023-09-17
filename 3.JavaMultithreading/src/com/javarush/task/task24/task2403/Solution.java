8adc15e3434f7d55d97e1327fc7f1e2e8c35f70b
Dmitriy <dmed832@yandex.ru>
2023-09-17 21:51:08 +0300

    add solution JavaMultithreading from task.task24.task2403

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2403/Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.task.task24.task2403;
+
+import java.io.Serializable;
+import java.rmi.Remote;
+import java.util.ArrayDeque;
+import java.util.EventListener;
+
+/* 
+Так-с... сопоставим
+*/
+
+public class Solution {
+    public static class A implements Serializable {
+    protected A clone() throws CloneNotSupportedException {
+        return (A) super.clone();
+    }
+    }
+
+    public static class B implements Remote {
+    protected final B clone() throws CloneNotSupportedException {
+        return (B) super.clone();
+    }
+    }
+
+    public static class C extends ArrayDeque {
+    public C clone() {
+        return (C) super.clone();
+    }
+    }
+
+    public static class D implements EventListener {
+    protected D clone() throws CloneNotSupportedException {
+        return (D) super.clone();
+    }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
