71a823082a13b8cc7799fb724d7433c738182426
Dmitriy <dmed832@yandex.ru>
2023-10-03 23:30:43 +0300

    add solution JavaMultithreading from task.task24.task2410

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2410/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task24.task2410;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/* 
+Рефакторинг, анонимные классы
+*/
+
+public class Solution {
+    public static List<Iterator> iterators = new LinkedList<>();
+
+    private int countItems;
+
+    public Iterator getIterator(final String name) {
+        return new Iterator() {
+          {
+                countItems++;
+                System.out.println(name + " item " + countItems);
+            }
+
+            public Iterator next() {
+                return getIterator(name);
+            }
+        };
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        Iterator iterator = solution.getIterator("iterator");
+        for (int i = 1; i < 5; i++) {
+            iterators.add(iterator.next());
+        }
+    }
+}
