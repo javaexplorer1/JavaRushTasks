602eef86ae85a39ba936105caa12ca045e213933
Dmitriy <dmed832@yandex.ru>
2023-10-08 23:58:21 +0300

    add solution JavaMultithreading from task.task25.task2504

---
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2504/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task25.task2504;
+
+/* 
+Switch для нитей
+*/
+
+public class Solution {
+    public static void processThreads(Thread... threads) {
+        for (Thread thread : threads) {
+            Thread.State state = thread.getState();
+            switch (state) {
+                case NEW:
+                    thread.start();
+                    break;
+                case WAITING:
+                case TIMED_WAITING:
+                case BLOCKED:
+                    thread.interrupt();
+                    break;
+                case RUNNABLE:
+                   thread.isInterrupted();
+                    break;
+                case TERMINATED:
+                    System.out.println(thread.getPriority());
+                    break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+    }
+}
