7df4f4bc492adc6cda4b41e65dc2a0757cb4e2a5
Dmitriy <dmed832@yandex.ru>
2023-10-12 00:00:28 +0300

    add solution JavaMultithreading from task.task25.task2505

---
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2505/Solution.java
@@ -0,0 +1,50 @@
+package com.javarush.task.task25.task2505;
+
+/* 
+Без дураков
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        MyThread myThread = new Solution().new MyThread("super secret key");
+        myThread.start();
+    }
+
+
+    public class MyThread extends Thread {
+        private String secretKey;
+
+        public MyThread(String secretKey) {
+            this.secretKey = secretKey;
+            setUncaughtExceptionHandler(new MyUncaughtExceptionHandler());
+            setDaemon(true);
+        }
+
+        @Override
+        public void run() {
+            throw new NullPointerException("it's an example");
+        }
+
+        private class MyUncaughtExceptionHandler implements UncaughtExceptionHandler {
+            public MyUncaughtExceptionHandler() {
+            }
+
+            public void uncaughtException(Thread t, Throwable e) {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException ex) {
+                    throw new RuntimeException(ex);
+                }
+                String format = String.format("%s, #s, %s", secretKey, t.getName(), e.getMessage());
+
+                System.out.println(format);
+
+            }
+
+    }
+
+
+
+}
+
