33b156d935e5cd5650e3289f3c4e2d67a6fd1026
Dmitriy <dmed832@yandex.ru>
2023-10-21 01:14:00 +0300

    add solution JavaMultithreading from task.task25.task2512 (refactored condition)

--- 3.JavaMultithreading/src/com/javarush/task/task25/task2512/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2512/Solution.java
@@ -13,15 +13,10 @@ public class Solution implements Thread.UncaughtExceptionHandler {
     public void uncaughtException(Thread t, Throwable e) {
         t.interrupt();
         Throwable cause = e.getCause();
-        if (cause == null) {
-            System.out.println(e.getClass().getName() + ": " + e.getMessage());
-            return;
+        if (cause != null) {
+            uncaughtException(t, e.getCause());
         }
-        uncaughtException(t, e.getCause());
         System.out.println(e.getClass().getName() + ": " + e.getMessage());
-
-
-
     }
 
     public static void main(String[] args) {
