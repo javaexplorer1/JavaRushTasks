176eb87abc491c2ed58980b2623ccef195c87424
Dmitriy <dmed832@yandex.ru>
2023-10-20 23:19:50 +0300

    add solution JavaMultithreading from task.task26.task2602

---
+++ 3.JavaMultithreading/src/com/javarush/task/task26/task2602/Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.task.task26.task2602;
+
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
+
+/* 
+Был бы ум - будет и успех
+*/
+
+public class Solution  {
+
+   public static void main(String[] args) {
+        Set<Soldier> soldiers = new TreeSet<>();
+        soldiers.add(new Soldier("Ivanov", 170));
+        soldiers.add(new Soldier("Petrov", 180));
+        soldiers.add(new Soldier("Sidorov", 175));
+
+        for (Soldier soldier : soldiers) {
+            System.out.println(soldier.name);
+        }
+    }
+
+    public static class Soldier implements Comparable<Soldier> {
+        private String name;
+        private int height;
+
+        public Soldier(String name, int height) {
+            this.name = name;
+            this.height = height;
+        }
+
+        @Override
+        public int compareTo(Soldier o) {
+            return o.height - this.height;
+        }
+    }
+}
