bd6b2bfdaec7f069df406636f966ef4d58088289
Dmitriy <dmed832@yandex.ru>
2023-10-26 00:45:33 +0300

    add solution JavaCore from task.jdk13.task38.task3801

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task38/task3801/LongPositive.java
@@ -0,0 +1,11 @@
+package com.javarush.task.jdk13.task38.task3801;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(value = ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LongPositive {
+}
