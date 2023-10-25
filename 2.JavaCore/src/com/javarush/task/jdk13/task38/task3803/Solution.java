fb37eb1880d603a846f17b6ef6ead719eccfe319
Dmitriy <dmed832@yandex.ru>
2023-10-26 00:59:19 +0300

    add solution JavaCore from task.jdk13.task38.task3803

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task38/task3803/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.jdk13.task38.task3803;
+
+/* 
+Обработка аннотаций
+*/
+
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+        printFullyQualifiedNames(Solution.class);
+        printFullyQualifiedNames(SomeTest.class);
+
+        printValues(Solution.class);
+        printValues(SomeTest.class);
+    }
+
+    public static <T> boolean printFullyQualifiedNames(Class<T> c) {
+        if (c.isAnnotationPresent(PrepareMyTest.class)) {
+            PrepareMyTest annotation = c.getAnnotation(PrepareMyTest.class);
+            System.out.println(annotation.fullyQualifiedNames()[0]);
+            Arrays.stream(annotation.fullyQualifiedNames()).forEach(System.out::println);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    public static <T> boolean printValues(Class<T> c) {
+        if (c.isAnnotationPresent(PrepareMyTest.class)) {
+            PrepareMyTest annotation = c.getAnnotation(PrepareMyTest.class);
+            Arrays.stream(annotation.value()).map(Class::getSimpleName).forEach(System.out::println);
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
