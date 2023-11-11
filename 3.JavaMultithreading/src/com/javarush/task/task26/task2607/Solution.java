1515ef7f74e34995df98b8323b18c35874444f1f
Dmitriy <dmed832@yandex.ru>
2023-11-11 23:53:11 +0300

    add solution JavaMultithreading from task.task26.task2607

---
+++ 3.JavaMultithreading/src/com/javarush/task/task26/task2607/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task26.task2607;
+
+/* 
+Вежливость - это искусственно созданное хорошее настроение
+*/
+
+public class Solution {
+
+    public static class IntegerHolder {
+
+        private int value;
+
+        public synchronized int get() {
+            return value;
+        }
+
+        public synchronized void set(int value) {
+            this.value = value;
+        }
+    }
+
+
+    public static void main(String[] args) {
+    }
+}
