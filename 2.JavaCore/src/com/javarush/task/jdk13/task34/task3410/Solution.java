9ab0129968b3cc4be7edb12196863f20c0642530
Dmitriy <dmed832@yandex.ru>
2023-12-02 23:53:04 +0300

    add solution JavaCore from task.jdk13.task34.task3410

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task34/task3410/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.jdk13.task34.task3410;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
+/* 
+Обнуление объекта
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        User user = new User(28219, "Paulo", 40, 9999, "paulo@javarush.com");
+        System.out.println(user);
+        reset(user);
+        System.out.println(user);
+    }
+
+    public static void reset(Object object) throws Exception {
+        for (Field field : object.getClass().getDeclaredFields()) {
+            if (Modifier.isPrivate(field.getModifiers()) &&
+                    !Modifier.isStatic(field.getModifiers()) &&
+                    !field.getType().isPrimitive()) {
+                field.setAccessible(true);
+                field.set(object, null);
+            }
+
+        }
+
+
+    }
+}
