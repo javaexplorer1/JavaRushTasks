49401a006560bdfe7383016eb1b4b3d3c6207ed0
Dmitriy <dmed832@yandex.ru>
2023-12-02 23:51:26 +0300

    add solution JavaCore from task.jdk13.task34.task3409

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task34/task3409/Generator.java
@@ -0,0 +1,20 @@
+package com.javarush.task.jdk13.task34.task3409;
+
+import java.lang.reflect.Constructor;
+
+public class Generator<T> {
+
+    private Class<T> aClass;
+
+    public Generator(Class<T> aClass) {
+        this.aClass = aClass;
+    }
+
+    @SuppressWarnings("unchecked")
+    T newInstance() throws Exception {
+        Constructor<T> declaredConstructor = (Constructor<T>) aClass.getDeclaredConstructors()[0];
+        int parameterCount = declaredConstructor.getParameterCount();
+        Object[] objects = new Object[parameterCount];
+        return declaredConstructor.newInstance(objects);
+    }
+}
