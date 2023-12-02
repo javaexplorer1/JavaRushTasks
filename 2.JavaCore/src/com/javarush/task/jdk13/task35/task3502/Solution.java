4fea8d37a1b488fbc1026fd81bff9db1ec6d9b2d
Dmitriy <dmed832@yandex.ru>
2023-12-02 23:57:31 +0300

    add solution JavaCore from task.jdk13.task35.task3502

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task35/task3502/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.jdk13.task35.task3502;
+
+import java.util.List;
+
+/* 
+Знакомство с дженериками
+*/
+
+public class Solution<T extends List<Solution.SomeClass>> {
+    public static void main(String[] args) {
+
+    }
+
+    public static class SomeClass<T extends Number> {
+    }
+}
