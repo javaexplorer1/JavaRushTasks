45547410a1eecbaf80c832af38d30db4df864afb
Dmitriy <dmed832@yandex.ru>
2023-12-09 01:33:28 +0300

    add solution JavaMultithreading from task.task27.task2701

---
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2701/Solution.java
@@ -0,0 +1,41 @@
+package com.javarush.task.task27.task2701;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+Избавляемся от меток
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String string = reader.readLine();
+        String substring = reader.readLine();
+
+        if (isSubstringPresent(substring, string)) {
+            System.out.println("String: \n" + substring + "\nis present in the string: \n" + string);
+        } else {
+            System.out.println("String: \n" + substring + "\nis not present in the string: \n" + string);
+        }
+    }
+
+    static boolean isSubstringPresent(String substring, String string) {
+        boolean found = false;
+        int max = string.length() - substring.length();
+        for (int i = 0; i <= max; i++) {
+            int length = substring.length();
+            int j = i;
+            int k = 0;
+            while ((length != 0) && (string.charAt(j++) == substring.charAt(k++))) {
+                length--;
+                if (length == 0) {
+                    found = true;
+                }
+            }
+        }
+        return found;
+    }
+}
+
