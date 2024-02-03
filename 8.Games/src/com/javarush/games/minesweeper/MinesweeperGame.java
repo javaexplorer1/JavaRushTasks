68b494187486d9ad32c7903efd26c913fe79a726
Dmitriy <dmed832@yandex.ru>
2024-02-03 21:29:40 +0300

    add solution Games from games.minesweeper Игра сапер (2/16)

--- 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
+++ 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
@@ -3,4 +3,13 @@ package com.javarush.games.minesweeper;
 import com.javarush.engine.cell.*;
 
 public class MinesweeperGame extends Game {
+
+    private static final int SIDE = 9;
+
+    @Override
+    public void initialize() {
+        setScreenSize(SIDE, SIDE);
+    }
+
+
 }
