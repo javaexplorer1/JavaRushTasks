dcff76c152fa1018df0ef144704102410ed9eb1c
Dmitriy <dmed832@yandex.ru>
2024-07-06 00:38:59 +0300

    add solution Games from games.snake Игра змейка (4/20)

--- 8.Games/src/com/javarush/games/snake/SnakeGame.java
+++ 8.Games/src/com/javarush/games/snake/SnakeGame.java
@@ -13,6 +13,8 @@ public class SnakeGame extends Game {
 
     private void createGame() {
         drawScene();
+        Apple apple = new Apple(7, 7);
+        apple.draw(this);
     }
 
     private void drawScene() {
