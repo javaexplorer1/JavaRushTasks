99512946ece6267dbe45eae6dcc732168211c895
Dmitriy <dmed832@yandex.ru>
2024-07-06 00:04:26 +0300

    add solution Games from games.snake Игра змейка (1/20)

---
+++ 8.Games/src/com/javarush/games/snake/SnakeGame.java
@@ -0,0 +1,12 @@
+package com.javarush.games.snake;
+
+import com.javarush.engine.cell.*;
+
+public class SnakeGame extends Game {
+    public static final int WIDTH = 15;
+    public static final int HEIGHT = 15;
+
+    public void initialize() {
+        setScreenSize(WIDTH, HEIGHT);
+    }
+}
